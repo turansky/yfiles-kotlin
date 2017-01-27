4a650793cee906848fff673517d08e475b9f91ca
Victor Turansky <victor.turansky@gmail.com>
2017-01-28 02:15:20 +0300

    Simple parse

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import org.gradle.api.GradleException
 import java.io.File
+import java.nio.charset.Charset
 
 task("build") {
     val source = project.properties["apiFile"] ?: throw GradleException("Invalid 'apiFile' parameter value!")
@@ -8,5 +9,46 @@ task("build") {
 }
 
 fun generateKotlinWrappers(sourceFile: File) {
-    println("Source file - '${sourceFile.absolutePath}'")
+    val lines = sourceFile.readLines(Charset.forName("UTF-8")).iterator()
+    val map = mutableMapOf<Declaration, Doc>()
+    var i = 0
+    while (lines.hasNext()) {
+        val doc = DocReader.read(lines)
+        val declaration = DeclarationReader.read(lines)
+        map.put(declaration, doc)
+    }
+
+    println("Declarations count: ${map.size}")
+}
+
+object DocReader {
+    fun read(lineIterator: Iterator<String>): Doc {
+        if (lineIterator.next() != "/**") {
+            throw IllegalStateException("Invalid comment start!")
+        }
+
+        val lines = mutableListOf<String>()
+        while (true) {
+            val line = lineIterator.next()
+            if (line == " */") {
+                return Doc(lines)
+            }
+
+            lines.add(line.substring(3))
+        }
+    }
+}
+
+object DeclarationReader {
+    fun read(lineIterator: Iterator<String>): Declaration {
+        return Declaration(lineIterator.next())
+    }
+}
+
+class Doc(private val lines: List<String>) {
+
+}
+
+class Declaration(private val data: String) {
+
 }
