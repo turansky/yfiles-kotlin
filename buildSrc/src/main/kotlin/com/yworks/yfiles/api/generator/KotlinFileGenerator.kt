d6d54cd105814befe9dca845f0d1cdc7c0a2b7a2
Victor Turansky <victor.turansky@gmail.com>
2018-07-22 01:42:27 +0300

    Encapsulation in KotlinFileGenerator.

--- buildSrc/src/main/kotlin/com/yworks/yfiles/api/generator/KotlinFileGenerator.kt
+++ buildSrc/src/main/kotlin/com/yworks/yfiles/api/generator/KotlinFileGenerator.kt
@@ -72,193 +72,193 @@ internal class KotlinFileGenerator(
 
         file.writeText("$header\n\n$content")
     }
-}
 
-private abstract class GeneratedFile(private val declaration: Type) {
-    val className = declaration.fqn
-    val fqn: FQN = FQN(className)
+    abstract inner class GeneratedFile(private val declaration: Type) {
+        val className = declaration.fqn
+        val fqn: FQN = FQN(className)
 
-    val properties: List<Property>
-        get() = declaration.properties
-                .sortedBy { it.name }
+        val properties: List<Property>
+            get() = declaration.properties
+                    .sortedBy { it.name }
 
-    val staticConstants: List<Constant>
-        get() = declaration.constants
-                .sortedBy { it.name }
+        val staticConstants: List<Constant>
+            get() = declaration.constants
+                    .sortedBy { it.name }
 
-    val staticProperties: List<Property>
-        get() = declaration.staticProperties
-                .sortedBy { it.name }
+        val staticProperties: List<Property>
+            get() = declaration.staticProperties
+                    .sortedBy { it.name }
 
-    val staticFunctions: List<Method>
-        get() = declaration.staticMethods
-                .sortedBy { it.name }
+        val staticFunctions: List<Method>
+            get() = declaration.staticMethods
+                    .sortedBy { it.name }
 
-    val staticDeclarations: List<Declaration>
-        get() {
-            return mutableListOf<Declaration>()
-                    .union(staticConstants)
-                    .union(staticProperties)
-                    .union(staticFunctions)
+        val staticDeclarations: List<Declaration>
+            get() {
+                return mutableListOf<Declaration>()
+                        .union(staticConstants)
+                        .union(staticProperties)
+                        .union(staticFunctions)
+                        .toList()
+            }
+
+        val memberProperties: List<Property>
+            get() = properties.filter { !it.static }
+
+        val memberFunctions: List<Method>
+            get() = declaration.methods
+                    .sortedBy { it.name }
+
+        val header: String
+            get() {
+                val module = findModule(declaration.modules)
+                val qualifier = getQualifier(fqn.packageName)
+                return "@file:JsModule(\"$module\")\n" +
+                        if (qualifier != null) {
+                            "@file:JsQualifier(\"$qualifier\")\n"
+                        } else {
+                            ""
+                        } +
+                        "package ${fqn.packageName}\n"
+            }
+
+        protected open fun parentTypes(): List<String> {
+            return declaration.implementedTypes()
+        }
+
+        protected fun parentString(): String {
+            val parentTypes = parentTypes()
+            if (parentTypes.isEmpty()) {
+                return ""
+            }
+            return ": " + parentTypes.joinToString(", ")
+        }
+
+        fun genericParameters(): String {
+            return declaration.genericParameters()
+        }
+
+        protected open fun isStatic(): Boolean {
+            return false
+        }
+
+        protected fun companionContent(): String {
+            val items = staticDeclarations.map {
+                it.toCode(PROGRAMMING_LANGUAGE)
+            }
+
+            if (items.isEmpty()) {
+                return ""
+            }
+
+            val result = items.joinToString("\n") + "\n"
+            if (isStatic()) {
+                return result
+            }
+
+            return "    companion object {\n" +
+                    result +
+                    "    }\n"
+        }
+
+        protected fun utilContent(): String {
+            val items = staticDeclarations.map {
+                it.toCode(PROGRAMMING_LANGUAGE)
+            }
+
+            if (items.isEmpty()) {
+                return ""
+            }
+
+            return "@JsName(\"$className\")\n" +
+                    "external object ${className}s {\n" +
+                    items.joinToString("\n") +
+                    "}"
+        }
+
+        open fun content(): String {
+            return listOf<Declaration>()
+                    .union(memberProperties)
+                    .union(memberFunctions)
+                    .map { it.toCode(PROGRAMMING_LANGUAGE) }
+                    .union(listOf(Hacks.getAdditionalContent(declaration.fqn)))
+                    .joinToString("\n") + "\n"
+        }
+    }
+
+    inner class ClassFile(private val declaration: Class) : GeneratedFile(declaration) {
+        override fun isStatic(): Boolean {
+            return declaration.static
+        }
+
+        private fun type(): String {
+            if (isStatic()) {
+                return "object"
+            }
+
+            val modificator = if (memberFunctions.any { it.abstract } || memberProperties.any { it.abstract }) {
+                "abstract"
+            } else {
+                declaration.modificator
+            }
+
+            return modificator + " class"
+        }
+
+        private fun constructors(): String {
+            val constructorSet = declaration.constructors.toSet()
+            return constructorSet.map {
+                it.toCode(PROGRAMMING_LANGUAGE)
+            }.joinToString("\n") + "\n"
+        }
+
+        override fun parentTypes(): List<String> {
+            val extendedType = declaration.extendedType()
+                    ?: return super.parentTypes()
+
+            return listOf(extendedType)
+                    .union(super.parentTypes())
                     .toList()
         }
 
-    val memberProperties: List<Property>
-        get() = properties.filter { !it.static }
-
-    val memberFunctions: List<Method>
-        get() = declaration.methods
-                .sortedBy { it.name }
-
-    val header: String
-        get() {
-            val module = findModule(declaration.modules)
-            val qualifier = getQualifier(fqn.packageName)
-            return "@file:JsModule(\"$module\")\n" +
-                    if (qualifier != null) {
-                        "@file:JsQualifier(\"$qualifier\")\n"
-                    } else {
-                        ""
-                    } +
-                    "package ${fqn.packageName}\n"
+        override fun content(): String {
+            return "external ${type()} ${fqn.name}${genericParameters()}${parentString()} {\n" +
+                    companionContent() +
+                    constructors() +
+                    super.content() + "\n" +
+                    "}"
         }
-
-    protected open fun parentTypes(): List<String> {
-        return declaration.implementedTypes()
     }
 
-    protected fun parentString(): String {
-        val parentTypes = parentTypes()
-        if (parentTypes.isEmpty()) {
-            return ""
+    inner class InterfaceFile(declaration: Interface) : GeneratedFile(declaration) {
+        override fun content(): String {
+            var content = super.content()
+            val likeAbstractClass = MixinHacks.defineLikeAbstractClass(className, memberFunctions, memberProperties)
+            if (!likeAbstractClass) {
+                content = content.replace("abstract ", "")
+                        .replace("open fun", "fun")
+                        .replace("\n    get() = definedExternally", "")
+                        .replace("\n    set(value) = definedExternally", "")
+                        .replace(" = definedExternally", "")
+            }
+
+            val type = if (likeAbstractClass) "abstract class" else "interface"
+            return "external $type ${fqn.name}${genericParameters()}${parentString()} {\n" +
+                    content + "\n" +
+                    "}\n\n" +
+                    utilContent()
         }
-        return ": " + parentTypes.joinToString(", ")
     }
 
-    fun genericParameters(): String {
-        return declaration.genericParameters()
-    }
-
-    protected open fun isStatic(): Boolean {
-        return false
-    }
-
-    protected fun companionContent(): String {
-        val items = staticDeclarations.map {
-            it.toCode(PROGRAMMING_LANGUAGE)
+    inner class EnumFile(private val declaration: Enum) : GeneratedFile(declaration) {
+        override fun content(): String {
+            val values = declaration.constants
+                    .map { "    ${it.name}" }
+                    .joinToString(",\n")
+            return "external enum class ${fqn.name} {\n" +
+                    values + "\n\n" +
+                    super.content() + "\n" +
+                    "}\n"
         }
-
-        if (items.isEmpty()) {
-            return ""
-        }
-
-        val result = items.joinToString("\n") + "\n"
-        if (isStatic()) {
-            return result
-        }
-
-        return "    companion object {\n" +
-                result +
-                "    }\n"
-    }
-
-    protected fun utilContent(): String {
-        val items = staticDeclarations.map {
-            it.toCode(PROGRAMMING_LANGUAGE)
-        }
-
-        if (items.isEmpty()) {
-            return ""
-        }
-
-        return "@JsName(\"$className\")\n" +
-                "external object ${className}s {\n" +
-                items.joinToString("\n") +
-                "}"
-    }
-
-    open fun content(): String {
-        return listOf<Declaration>()
-                .union(memberProperties)
-                .union(memberFunctions)
-                .map { it.toCode(PROGRAMMING_LANGUAGE) }
-                .union(listOf(Hacks.getAdditionalContent(declaration.fqn)))
-                .joinToString("\n") + "\n"
-    }
-}
-
-private class ClassFile(private val declaration: Class) : GeneratedFile(declaration) {
-    override fun isStatic(): Boolean {
-        return declaration.static
-    }
-
-    private fun type(): String {
-        if (isStatic()) {
-            return "object"
-        }
-
-        val modificator = if (memberFunctions.any { it.abstract } || memberProperties.any { it.abstract }) {
-            "abstract"
-        } else {
-            declaration.modificator
-        }
-
-        return modificator + " class"
-    }
-
-    private fun constructors(): String {
-        val constructorSet = declaration.constructors.toSet()
-        return constructorSet.map {
-            it.toCode(PROGRAMMING_LANGUAGE)
-        }.joinToString("\n") + "\n"
-    }
-
-    override fun parentTypes(): List<String> {
-        val extendedType = declaration.extendedType()
-                ?: return super.parentTypes()
-
-        return listOf(extendedType)
-                .union(super.parentTypes())
-                .toList()
-    }
-
-    override fun content(): String {
-        return "external ${type()} ${fqn.name}${genericParameters()}${parentString()} {\n" +
-                companionContent() +
-                constructors() +
-                super.content() + "\n" +
-                "}"
-    }
-}
-
-private class InterfaceFile(declaration: Interface) : GeneratedFile(declaration) {
-    override fun content(): String {
-        var content = super.content()
-        val likeAbstractClass = MixinHacks.defineLikeAbstractClass(className, memberFunctions, memberProperties)
-        if (!likeAbstractClass) {
-            content = content.replace("abstract ", "")
-                    .replace("open fun", "fun")
-                    .replace("\n    get() = definedExternally", "")
-                    .replace("\n    set(value) = definedExternally", "")
-                    .replace(" = definedExternally", "")
-        }
-
-        val type = if (likeAbstractClass) "abstract class" else "interface"
-        return "external $type ${fqn.name}${genericParameters()}${parentString()} {\n" +
-                content + "\n" +
-                "}\n\n" +
-                utilContent()
-    }
-}
-
-private class EnumFile(private val declaration: Enum) : GeneratedFile(declaration) {
-    override fun content(): String {
-        val values = declaration.constants
-                .map { "    ${it.name}" }
-                .joinToString(",\n")
-        return "external enum class ${fqn.name} {\n" +
-                values + "\n\n" +
-                super.content() + "\n" +
-                "}\n"
     }
 }
