ddbf38fc0c1aca610b2cb7866b132d19e2c771fb
Victor Turansky <victor.turansky@gmail.com>
2019-04-27 10:18:52 +0300

    Add test app

---
+++ test-app/src/main/kotlin/App.kt
@@ -0,0 +1,27 @@
+import yfiles.graph.DefaultGraph
+import yfiles.hierarchic.HierarchicLayout
+import yfiles.input.GraphViewerInputMode
+import yfiles.view.GraphComponent
+import org.w3c.dom.HTMLDivElement
+
+fun create(): HTMLDivElement {
+    val component = GraphComponent()
+    component.inputMode = GraphViewerInputMode()
+
+    val container = component.div
+    container.style.width = "100%"
+    container.style.height = "100%"
+    container.style.backgroundColor = "#CCCCCC"
+
+    val graph = DefaultGraph()
+    val node1 = graph.createNode()
+    val node2 = graph.createNode()
+    graph.createEdge(node1, node2)
+
+    graph.applyLayout(HierarchicLayout())
+
+    component.graph = graph
+    component.fitGraphBounds()
+
+    return container
+}
