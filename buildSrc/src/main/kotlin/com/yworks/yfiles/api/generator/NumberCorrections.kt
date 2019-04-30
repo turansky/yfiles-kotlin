2f25f9f2c5ae993c801e78a7629cc9841fb75e9e
Victor Turansky <victor.turansky@gmail.com>
2019-04-30 12:05:29 +0300

    Number corrections #3

--- buildSrc/src/main/kotlin/com/yworks/yfiles/api/generator/NumberCorrections.kt
+++ buildSrc/src/main/kotlin/com/yworks/yfiles/api/generator/NumberCorrections.kt
@@ -2,8 +2,11 @@ package com.yworks.yfiles.api.generator
 
 import org.json.JSONObject
 
+private val INT = "Int"
+private val DOUBLE = "Double"
+
 internal fun correctNumbers(source: JSONObject) {
-    val types = source.getJSONArray("namespaces")
+    source.getJSONArray("namespaces")
         .asSequence()
         .map { it as JSONObject }
         .filter { it.has("namespaces") }
@@ -11,21 +14,31 @@ internal fun correctNumbers(source: JSONObject) {
         .map { it as JSONObject }
         .flatMap { it.getJSONArray("types").asSequence() }
         .map { it as JSONObject }
-        .toList()
-
-    val numberProperties = types
-        .asSequence()
-        .flatMap { it.correctProperties() }
-        .toSet()
+        .forEach { it.correctProperties() }
 }
 
-private fun JSONObject.correctProperties() =
-    if (has("properties")) {
+private fun JSONObject.correctProperties() {
+    if (!has("properties")) {
+        return
+    }
+
+    val className = getString("name")
         getJSONArray("properties")
             .asSequence()
             .map { it as JSONObject }
             .filter { it.getString("type") == JS_NUMBER }
-            .map { it.getString("name") }
-    } else {
-        emptySequence()
+            .forEach { it.put("type", getPropertyType(className, it.getString("name"))) }
+}
+
+private fun getPropertyType(className: String, propertyName: String): String {
+    if (className == "AffineLine" && (propertyName == "a" || propertyName == "b")) {
+        return DOUBLE
     }
+
+    return when (propertyName) {
+        in INT_PROPERTIES -> INT
+        in DOUBLE_PROPERTIES -> DOUBLE
+        else -> throw IllegalStateException("Unexpected $className.$propertyName")
+    }
+}
+
