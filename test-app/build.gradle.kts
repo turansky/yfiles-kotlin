50640a22c98b05d9754751495effaca2c48c0788
Victor Turansky <victor.turansky@gmail.com>
2019-06-01 13:13:15 +0300

    Formatting

--- test-app/build.gradle.kts
+++ test-app/build.gradle.kts
@@ -1,12 +1,12 @@
-import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
-
 plugins {
     id("kotlin2js")
 }
 
 tasks {
-    "compileKotlin2Js"(Kotlin2JsCompile::class) {
-        kotlinOptions.moduleKind = "amd"
+    compileKotlin2Js {
+        kotlinOptions {
+            moduleKind = "amd"
+        }
     }
 }
 
