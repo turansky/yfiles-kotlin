5f725f02f7a554520d7d595c91cc62c97cf090f8
Victor Turansky <victor.turansky@gmail.com>
2019-07-08 00:52:00 +0300

    YSubplugin

---
+++ plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/YSubplugin.kt
@@ -0,0 +1,40 @@
+package com.github.turansky.yfiles.gradle.plugin
+
+import com.google.auto.service.AutoService
+import org.gradle.api.Project
+import org.gradle.api.tasks.compile.AbstractCompile
+import org.jetbrains.kotlin.gradle.dsl.KotlinCommonOptions
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
+import org.jetbrains.kotlin.gradle.plugin.KotlinGradleSubplugin
+import org.jetbrains.kotlin.gradle.plugin.SubpluginArtifact
+import org.jetbrains.kotlin.gradle.plugin.SubpluginOption
+
+@AutoService(KotlinGradleSubplugin::class)
+class YSubplugin : KotlinGradleSubplugin<AbstractCompile> {
+
+    override fun isApplicable(project: Project, task: AbstractCompile) =
+        project.plugins.hasPlugin(YPlugin::class.java)
+
+    override fun apply(
+        project: Project,
+        kotlinCompile: AbstractCompile,
+        javaCompile: AbstractCompile?,
+        variantData: Any?,
+        androidProjectHandler: Any?,
+        kotlinCompilation: KotlinCompilation<KotlinCommonOptions>?
+    ): List<SubpluginOption> {
+        return emptyList()
+    }
+
+    /**
+     * Just needs to be consistent with the key for DebugLogCommandLineProcessor#pluginId
+     */
+    override fun getCompilerPluginId(): String = "yfiles-kotlin"
+
+    override fun getPluginArtifact(): SubpluginArtifact =
+        SubpluginArtifact(
+            groupId = "com.github.turansky.yfiles",
+            artifactId = "kotlin-plugin",
+            version = "0.0.1"
+        )
+}
