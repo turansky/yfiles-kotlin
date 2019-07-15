615787e9bd34581696ecb63d70960d86c9eb3f4d
Victor Turansky <victor.turansky@gmail.com>
2019-07-15 03:47:27 +0300

    Api links #7

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/Wrappers.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/Wrappers.kt
@@ -554,12 +554,14 @@ private class SummaryDelegate {
         val memberTextRegex = Regex("<api-link data-type=\"([a-zA-Z.]+)\" data-member=\"([a-zA-Z0-9_]+)\" data-text=\"([^\"]+)\"></api-link>")
         return this
             .replace(" data-text=\"\"", "")
+            .replace(" infinite\"\"=\"\"", "")
             .replace(typeCleanRegex1, "$1")
             .replace(typeCleanRegex2, "$1")
             .replace(typeRegex, "[$1]")
             .replace(typeTextRegex, "[$2][$1]")
             .replace(memberRegex, "[$1.$2]")
             .replace(memberTextRegex, "[$3][$1.$2]")
+            .also { check(!it.contains("<api-link")) }
     }
 
     private fun String.fixMarkdown(): String =
