c17c201307eadbf6c34b690abdb8d481912c1bb9
Victor Turansky <victor.turansky@gmail.com>
2019-07-20 03:58:57 +0300

    "as" -> "opt"

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/CastExtensions.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/CastExtensions.kt
@@ -12,7 +12,7 @@ fun interfaceCastExtensions(
         |   return ${yclass}.isInstance(this)
         |}
         |
-        |inline fun $generics Any?.as$className(): $classDeclaration? {
+        |inline fun $generics Any?.opt$className(): $classDeclaration? {
         |   return if ( is$className() ) {
         |       unsafeCast<$classDeclaration>()
         |   } else {
@@ -21,7 +21,7 @@ fun interfaceCastExtensions(
         |}
         |
         |inline fun $generics Any?.to$className(): $classDeclaration {
-        |   return as$className()!!
+        |   return opt$className()!!
         |}
     """.trimMargin()
 }
