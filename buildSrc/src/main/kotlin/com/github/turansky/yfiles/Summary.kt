298c1ea489e506a531f05aba6cade2addf6fd0a6
Victor Turansky <victor.turansky@gmail.com>
2019-07-20 16:11:01 +0300

    Formatting

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/Summary.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/Summary.kt
@@ -8,8 +8,10 @@ internal fun summary(source: String): String {
 
 private val TYPE_CLEAN_REGEX_1 = Regex("( data-type=\"[a-zA-Z.]+)<[^\"]+")
 private val TYPE_CLEAN_REGEX_2 = Regex("( data-type=\"[a-zA-Z.]+)&lt;[^\"]+")
+
 private val TYPE_REGEX = Regex("<api-link data-type=\"([a-zA-Z0-9.]+)\"></api-link>")
 private val TYPE_TEXT_REGEX = Regex("<api-link data-type=\"([a-zA-Z0-9.]+)\" data-text=\"([^\"]+)\"></api-link>")
+
 private val MEMBER_REGEX = Regex("<api-link data-type=\"([a-zA-Z.]+)\" data-member=\"([a-zA-Z0-9_]+)\"></api-link>")
 private val MEMBER_TEXT_REGEX = Regex("<api-link data-type=\"([a-zA-Z.]+)\" data-member=\"([a-zA-Z0-9_]+)\" data-text=\"([^\"]+)\"></api-link>")
 
