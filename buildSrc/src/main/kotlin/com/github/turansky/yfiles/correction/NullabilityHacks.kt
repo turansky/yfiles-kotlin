939aa6c14db581fda35fdaf8ea881b29f12d5616
Victor Turansky <victor.turansky@gmail.com>
2019-07-31 18:17:07 +0300

    Router. Fix methods nullability #1

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/NullabilityHacks.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/NullabilityHacks.kt
@@ -7,6 +7,7 @@ internal fun fixNullability(source: Source) {
     fixLayoutNullability(source)
     fixStageNullability(source)
     fixHierarchicNullability(source)
+    fixRouterNullability(source)
     fixTreeNullability(source)
 }
 
@@ -313,6 +314,45 @@ private fun fixHierarchicNullability(source: Source) {
         "SimplexNodePlacer",
 
         "SelfLoopCalculator"
+    ).flatMap { getAffectedMethods(it) }
+        .filter { it.has(J_PARAMETERS) }
+        .jsequence(J_PARAMETERS)
+        .filterNot { it.getString(J_NAME) in EXCLUDED_PARAMETERS }
+        .filterNot { it.getString(J_TYPE) in EXCLUDED_TYPES }
+        .forEach { it.changeNullability(false) }
+}
+
+private fun fixRouterNullability(source: Source) {
+    val EXCLUDED_METHODS = setOf(
+        "applyLayout",
+        "applyLayoutCore"
+    )
+
+    val EXCLUDED_TYPES = setOf(
+        "boolean",
+        "number"
+    )
+
+    val EXCLUDED_PARAMETERS = setOf<String>()
+
+    source.types(
+        "BusRepresentations"
+    ).jsequence(J_STATIC_METHODS)
+        .filter { it.has(J_PARAMETERS) }
+        .filterNot { it.getString(J_NAME) in EXCLUDED_METHODS }
+        .jsequence(J_PARAMETERS)
+        .filterNot { it.getString(J_TYPE) in EXCLUDED_TYPES }
+        .forEach { it.changeNullability(false) }
+
+    fun getAffectedMethods(type: JSONObject): Sequence<JSONObject> =
+        (type.jsequence(J_METHODS) + type.optJsequence(J_STATIC_METHODS))
+            .filterNot { it.getString(J_NAME) in EXCLUDED_METHODS }
+
+    source.types(
+        "ChannelEdgeRouter",
+        "EdgeRouter",
+        "OrthogonalPatternEdgeRouter",
+        "ParallelEdgeRouter"
     ).flatMap { getAffectedMethods(it) }
         .filter { it.has(J_PARAMETERS) }
         .jsequence(J_PARAMETERS)
