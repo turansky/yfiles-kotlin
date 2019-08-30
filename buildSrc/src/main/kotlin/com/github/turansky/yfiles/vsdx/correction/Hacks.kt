eea9bf889fe5bb302e0e17160e78b32e3936f18a
Victor Turansky <victor.turansky@gmail.com>
2019-08-31 00:07:47 +0300

    VSDX. Temp remove static methods

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/vsdx/correction/Hacks.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/vsdx/correction/Hacks.kt
@@ -2,12 +2,14 @@ package com.github.turansky.yfiles.vsdx.correction
 
 import com.github.turansky.yfiles.correction.J_IMPLEMENTS
 import com.github.turansky.yfiles.correction.J_METHODS
+import com.github.turansky.yfiles.correction.J_STATIC_METHODS
 import org.json.JSONObject
 
 internal fun applyVsdxHacks(api: JSONObject) {
     val source = VsdxSource(api)
 
     source.types()
+        .onEach { it.remove(J_STATIC_METHODS) }
         .onEach { it.remove(J_METHODS) }
         .forEach { it.remove(J_IMPLEMENTS) }
 }
