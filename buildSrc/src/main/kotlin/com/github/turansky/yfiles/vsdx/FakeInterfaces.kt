b3064a64a65a7c6d38fda8c646aa9395dbd17383
Victor Turansky <victor.turansky@gmail.com>
2019-08-31 04:17:28 +0300

    VSDX. Add fake yFiles types #2

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/vsdx/FakeInterfaces.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/vsdx/FakeInterfaces.kt
@@ -8,21 +8,27 @@ import com.github.turansky.yfiles.json.jObject
 
 internal fun fakeVsdxInterfaces(): List<Interface> {
     return listOf(
-        fakeVsdxInterface("yfiles.collections.IEnumerable", "getEnumerator"),
-        fakeVsdxInterface("yfiles.collections.IListEnumerable", "get")
+        fakeVsdxInterface(
+            id = "yfiles.collections.IEnumerable",
+            methodNames = setOf("getEnumerator")
+        ),
+        fakeVsdxInterface(
+            id = "yfiles.collections.IListEnumerable",
+            methodNames = setOf("getEnumerator", "get")
+        )
     )
 }
 
-private fun fakeVsdxInterface(id: String, methodName: String): Interface {
+private fun fakeVsdxInterface(id: String, methodNames: Set<String>): Interface {
     return Interface(
         jObject(
             J_ID to id,
             J_NAME to id,
-            J_METHODS to arrayOf(
+            J_METHODS to methodNames.map {
                 jObject(
-                    J_NAME to methodName
+                    J_NAME to it
                 )
-            )
+            }
         )
     )
 }
