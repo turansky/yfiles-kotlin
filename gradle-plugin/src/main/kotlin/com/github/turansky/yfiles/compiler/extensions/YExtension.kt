0c60b918812f35c237b4f050e8544e83cf6e592e
Victor Turansky <victor.turansky@gmail.com>
2019-09-14 17:00:39 +0300

    Plugin. Disable yFiles interface implementing for non-external classes

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/extensions/YExtension.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/extensions/YExtension.kt
@@ -1,10 +1,19 @@
 package com.github.turansky.yfiles.compiler.extensions
 
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.js.translate.context.TranslationContext
 import org.jetbrains.kotlin.js.translate.declaration.DeclarationBodyVisitor
 import org.jetbrains.kotlin.js.translate.extensions.JsSyntheticTranslateExtension
 import org.jetbrains.kotlin.psi.KtPureClassOrObject
+import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
+import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameUnsafe
+import org.jetbrains.kotlin.resolve.descriptorUtil.getSuperInterfaces
+
+private val AFFECTED_CLASS_KINDS = setOf(
+    ClassKind.CLASS,
+    ClassKind.OBJECT
+)
 
 class YExtension : JsSyntheticTranslateExtension {
     override fun generateClassSyntheticParts(
@@ -13,6 +22,44 @@ class YExtension : JsSyntheticTranslateExtension {
         translator: DeclarationBodyVisitor,
         context: TranslationContext
     ) {
-        println("Generate:" + declaration.name)
+        if (descriptor.kind !in AFFECTED_CLASS_KINDS) {
+            return
+        }
+
+        if (descriptor.isExternal) {
+            return
+        }
+
+        val superInterfaces = descriptor.getSuperInterfaces()
+        if (superInterfaces.isEmpty()) {
+            return
+        }
+
+        val externalSuperInterfaces = superInterfaces
+            .filter { it.isExternal }
+
+        if (externalSuperInterfaces.isEmpty()) {
+            return
+        }
+
+        val yfilesInterfaces = externalSuperInterfaces
+            .filter { it.companionObjectDescriptor != null }
+            .mapNotNull { it.fqNameOrNull() }
+            .map { it.asString() }
+            .filter { it.startsWith("yfiles.") }
+            .toList()
+
+        if (yfilesInterfaces.isEmpty()) {
+            return
+        }
+
+        val type = descriptor.fqNameUnsafe.asString()
+        val reason = """
+            | $type couldn't implement following interfaces: ${yfilesInterfaces.joinToString()}.
+            | yFiles interface implementation not supported for non-external classes yet.
+        """.trimMargin()
+
+        // TODO: throw compiler error instead
+        TODO(reason)
     }
 }
