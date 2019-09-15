0e56aa2bf8967b4c868d963c52e0dbc308c9e20c
Victor Turansky <victor.turansky@gmail.com>
2019-09-16 02:05:05 +0300

    Plugin. Add messages extension

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/diagnostic/YError.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/diagnostic/YError.kt
@@ -4,5 +4,5 @@ import org.jetbrains.kotlin.diagnostics.DiagnosticFactory0
 import org.jetbrains.kotlin.diagnostics.Severity
 import org.jetbrains.kotlin.psi.KtElement
 
-val YFILES_INTERFACE_IMPLEMENTING_NOT_SUPPORTED: DiagnosticFactory0<KtElement> =
+internal val YFILES_INTERFACE_IMPLEMENTING_NOT_SUPPORTED: DiagnosticFactory0<KtElement> =
     DiagnosticFactory0.create(Severity.ERROR)
---
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/diagnostic/YMessagesExtension.kt
@@ -0,0 +1,18 @@
+package com.github.turansky.yfiles.compiler.diagnostic
+
+import org.jetbrains.kotlin.diagnostics.rendering.DefaultErrorMessages
+import org.jetbrains.kotlin.diagnostics.rendering.DiagnosticFactoryToRendererMap
+
+private val DIAGNOSTIC_FACTORY_TO_RENDERER by lazy {
+    DiagnosticFactoryToRendererMap("yfiles").apply {
+        put(
+            YFILES_INTERFACE_IMPLEMENTING_NOT_SUPPORTED,
+            "Non-external type couldn't implement yFiles interface(s)"
+        )
+    }
+}
+
+internal object YMessagesExtension : DefaultErrorMessages.Extension {
+    override fun getMap(): DiagnosticFactoryToRendererMap =
+        DIAGNOSTIC_FACTORY_TO_RENDERER
+}
