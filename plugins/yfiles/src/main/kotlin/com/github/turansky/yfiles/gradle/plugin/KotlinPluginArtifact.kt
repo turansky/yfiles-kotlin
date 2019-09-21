d594a2456e6b09e0c9be0cf001a2a91948652bb7
Victor Turansky <victor.turansky@gmail.com>
2019-09-22 01:59:41 +0300

    Plugin. Version - 0.0.8-SNAPSHOT

--- plugins/yfiles/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/KotlinPluginArtifact.kt
+++ plugins/yfiles/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/KotlinPluginArtifact.kt
@@ -6,5 +6,5 @@ internal val KOTLIN_PLUGIN_ARTIFACT: SubpluginArtifact
     get() = SubpluginArtifact(
         groupId = "gradle.plugin.com.github.turansky.yfiles",
         artifactId = "yfiles",
-        version = "0.0.7-SNAPSHOT"
+        version = "0.0.8-SNAPSHOT"
     )
