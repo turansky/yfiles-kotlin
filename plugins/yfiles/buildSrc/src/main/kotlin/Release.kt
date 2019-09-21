5cdcf8c1aae3aeae23ecfd8b62131dc285720cfb
Victor Turansky <victor.turansky@gmail.com>
2019-09-22 02:42:46 +0300

    Plugin. Add group correction during release

--- plugins/yfiles/buildSrc/src/main/kotlin/Release.kt
+++ plugins/yfiles/buildSrc/src/main/kotlin/Release.kt
@@ -1,6 +1,7 @@
 import org.gradle.api.Project
 import java.io.File
 
+private val GRADLE_PLUGIN_PREFIX = "gradle.plugin."
 private val ARTIFACT_PATH = "com/github/turansky/yfiles/gradle/plugin/KotlinPluginArtifact.kt"
 
 fun Project.preparePublish(sourceDir: File) {
@@ -8,6 +9,7 @@ fun Project.preparePublish(sourceDir: File) {
     val releaseVersion = version.copy(snapshot = false)
 
     changeVersion(sourceDir, version, releaseVersion)
+    changeGroup(false)
 }
 
 fun Project.prepareDevelopment(sourceDir: File) {
@@ -18,6 +20,18 @@ fun Project.prepareDevelopment(sourceDir: File) {
     )
 
     changeVersion(sourceDir, version, snapshotVersion)
+    changeGroup(true)
+}
+
+private fun Project.changeGroup(addPrefix: Boolean) {
+    var group = group.toString()
+    group = if (addPrefix) {
+        "$GRADLE_PLUGIN_PREFIX$group"
+    } else {
+        group.removePrefix(GRADLE_PLUGIN_PREFIX)
+    }
+
+    changeProperty("group", group)
 }
 
 internal fun Project.readVersion(): Version =
