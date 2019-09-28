9da17695f06751a2c10f4e0a0774a8bc17782033
Victor Turansky <victor.turansky@gmail.com>
2019-09-29 02:13:12 +0300

    Plugin Test. Micro update

--- plugin-tests/yfiles/src/main/kotlin/MyVisibilityTestable.kt
+++ plugin-tests/yfiles/src/main/kotlin/MyVisibilityTestable.kt
@@ -2,7 +2,7 @@ import yfiles.geometry.Rect
 import yfiles.view.ICanvasContext
 import yfiles.view.IVisibilityTestable
 
-interface MyVisibilityTestable: IVisibilityTestable {
+class MyVisibilityTestable : IVisibilityTestable {
     override fun isVisible(
         context: ICanvasContext,
         rectangle: Rect
