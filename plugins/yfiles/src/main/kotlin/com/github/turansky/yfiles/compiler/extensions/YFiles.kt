7077d36e3d92023c22c608b7a65d4ac45b93da54
Victor Turansky <victor.turansky@gmail.com>
2019-09-29 00:48:21 +0300

    Plugin. Separate ClassDescriptor check

---
+++ plugins/yfiles/src/main/kotlin/com/github/turansky/yfiles/compiler/extensions/YFiles.kt
@@ -0,0 +1,22 @@
+package com.github.turansky.yfiles.compiler.extensions
+
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.name.Name.identifier
+import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
+
+private val YFILES_PACKAGE = identifier("yfiles")
+
+fun ClassDescriptor.isYFiles(): Boolean {
+    if (companionObjectDescriptor != null) {
+        return false
+    }
+
+    val fqName = fqNameOrNull()
+        ?: return false
+
+    if (fqName.isRoot) {
+        return false
+    }
+
+    return fqName.pathSegments().first() == YFILES_PACKAGE
+}
