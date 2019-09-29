d725155039294b06b9b5d7963f234d3d6e11b190
Victor Turansky <victor.turansky@gmail.com>
2019-09-30 00:04:34 +0300

    Plugin. Factory method for DiagnosticFactory0

--- plugins/yfiles/src/main/kotlin/com/github/turansky/yfiles/compiler/diagnostic/YError.kt
+++ plugins/yfiles/src/main/kotlin/com/github/turansky/yfiles/compiler/diagnostic/YError.kt
@@ -4,29 +4,33 @@ import org.jetbrains.kotlin.diagnostics.DiagnosticFactory0
 import org.jetbrains.kotlin.diagnostics.Severity.ERROR
 import org.jetbrains.kotlin.psi.KtElement
 
-internal val BASE_CLASS__INTERFACE_IMPLEMENTING_NOT_SUPPORTED: DiagnosticFactory0<KtElement> =
+private fun <T : KtElement> errorDiagnosticFactory(): DiagnosticFactory0<T> =
     DiagnosticFactory0.create(ERROR)
+
+internal val BASE_CLASS__INTERFACE_IMPLEMENTING_NOT_SUPPORTED: DiagnosticFactory0<KtElement> =
+    errorDiagnosticFactory()
 
 internal val BASE_CLASS__INTERFACE_MIXING_NOT_SUPPORTED: DiagnosticFactory0<KtElement> =
-    DiagnosticFactory0.create(ERROR)
+    errorDiagnosticFactory()
 
 internal val BASE_CLASS__CONSTRUCTOR_NOT_SUPPORTED: DiagnosticFactory0<KtElement> =
-    DiagnosticFactory0.create(ERROR)
+    errorDiagnosticFactory()
 
 internal val BASE_CLASS__BODY_NOT_SUPPORTED: DiagnosticFactory0<KtElement> =
-    DiagnosticFactory0.create(ERROR)
+    errorDiagnosticFactory()
 
 internal val BASE_CLASS__INLINE_CLASS_NOT_SUPPORTED: DiagnosticFactory0<KtElement> =
-    DiagnosticFactory0.create(ERROR)
+    errorDiagnosticFactory()
 
 internal val BASE_CLASS__DATA_CLASS_NOT_SUPPORTED: DiagnosticFactory0<KtElement> =
-    DiagnosticFactory0.create(ERROR)
+    errorDiagnosticFactory()
 
 internal val BASE_CLASS__COMPANION_OBJECT_NOT_SUPPORTED: DiagnosticFactory0<KtElement> =
-    DiagnosticFactory0.create(ERROR)
+    errorDiagnosticFactory()
+
 
 internal val YOBJECT__INTERFACE_IMPLEMENTING_NOT_SUPPORTED: DiagnosticFactory0<KtElement> =
-    DiagnosticFactory0.create(ERROR)
+    errorDiagnosticFactory()
 
 internal val YOBJECT__COMPANION_OBJECT_NOT_SUPPORTED: DiagnosticFactory0<KtElement> =
-    DiagnosticFactory0.create(ERROR)
+    errorDiagnosticFactory()
