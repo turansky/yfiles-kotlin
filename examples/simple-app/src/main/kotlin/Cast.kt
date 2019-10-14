44831c6db1203c4ea8cd9bbcc660f987eb0e50b2
Victor Turansky <victor.turansky@gmail.com>
2019-10-15 00:17:43 +0300

    Add 'Cast' example

---
+++ examples/simple-app/src/main/kotlin/Cast.kt
@@ -0,0 +1,17 @@
+import yfiles.graph.DefaultGraph
+import yfiles.graph.IGraph
+import yfiles.graph.IModelItem
+import yfiles.graph.INode
+import yfiles.lang.yAs
+import yfiles.lang.yIs
+import yfiles.lang.yTo
+
+fun cast() {
+    val g: IGraph = DefaultGraph()
+    val n1: IModelItem = g.createNode()
+    val n2: INode = g.createNode()
+
+    val isNode: Boolean = n1 yIs INode
+    val asNode: INode? = n1 yAs INode
+    val toNode: INode = n1 yTo INode
+}
