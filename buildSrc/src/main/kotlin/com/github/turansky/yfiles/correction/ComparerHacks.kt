42474a375cf286170162be62390532c2fc074075
Victor Turansky <victor.turansky@gmail.com>
2019-10-19 15:57:36 +0300

    Empty IComparer parameter correction

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/ComparerHacks.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/ComparerHacks.kt
@@ -1,6 +1,7 @@
 package com.github.turansky.yfiles.correction
 
 import com.github.turansky.yfiles.JS_ANY
+import com.github.turansky.yfiles.JS_OBJECT
 import com.github.turansky.yfiles.json.firstWithName
 import org.json.JSONObject
 
@@ -8,7 +9,10 @@ private val NODE = "yfiles.algorithms.Node"
 private val EDGE = "yfiles.algorithms.Edge"
 private val GRAPH_OBJECT = "yfiles.algorithms.GraphObject"
 
-private val DEFAULT_COMPARER = comparer(JS_ANY)
+private val DEFAULT_COMPARERS = setOf(
+    comparer(JS_ANY),
+    comparer(JS_OBJECT)
+)
 
 private fun comparer(generic: String): String =
     "yfiles.collections.IComparer<$generic>"
@@ -16,6 +20,7 @@ private fun comparer(generic: String): String =
 internal fun applyComparerHacks(source: Source) {
     fixComparerInheritors(source)
     fixComparerUtilMethods(source)
+    fixComparerAsMethodParameter(source)
     fixNodePlacers(source)
 }
 
@@ -29,7 +34,7 @@ private fun fixComparerInheritors(source: Source) {
             .apply {
                 getJSONArray(J_IMPLEMENTS).apply {
                     require(length() == 1)
-                    require(get(0) == DEFAULT_COMPARER)
+                    require(get(0) in DEFAULT_COMPARERS)
 
                     put(0, comparer(generic))
                 }
@@ -60,6 +65,25 @@ private fun fixComparerUtilMethods(source: Source) {
     }
 }
 
+private fun fixComparerAsMethodParameter(source: Source) {
+    source.types(
+        "Graph",
+        "YNode"
+    ).jsequence(J_METHODS)
+        .forEach {
+            val methodName = it.getString(J_NAME)
+
+            it.optJsequence(J_PARAMETERS)
+                .filter { it.getString(J_TYPE) in DEFAULT_COMPARERS }
+                .forEach { it.fixTypeGeneric(getGeneric(methodName, it.getString(J_NAME))) }
+        }
+}
+
+private fun getGeneric(methodName: String, parameterName: String): String {
+    println(methodName + " : " + parameterName)
+    return JS_ANY
+}
+
 private fun fixNodePlacers(source: Source) {
     source.types(
         "IFromSketchNodePlacer",
@@ -87,8 +111,12 @@ private fun fixNodePlacers(source: Source) {
 }
 
 private fun JSONObject.fixReturnTypeGeneric(generic: String) {
-    getJSONObject(J_RETURNS).apply {
-        require(getString(J_TYPE) == DEFAULT_COMPARER)
-        put(J_TYPE, comparer(generic))
-    }
+    getJSONObject(J_RETURNS)
+        .fixTypeGeneric(generic)
+}
+
+private fun JSONObject.fixTypeGeneric(generic: String) {
+    require(getString(J_TYPE) in DEFAULT_COMPARERS)
+
+    put(J_TYPE, comparer(generic))
 }
