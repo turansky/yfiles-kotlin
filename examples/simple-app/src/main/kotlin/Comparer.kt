31e5bf620eababf0812768566bac3c3dc96c147f
Victor Turansky <victor.turansky@gmail.com>
2019-10-20 00:50:09 +0300

    Add IComparer check #2

--- examples/simple-app/src/main/kotlin/Comparer.kt
+++ examples/simple-app/src/main/kotlin/Comparer.kt
@@ -3,10 +3,15 @@
 import yfiles.algorithms.Comparers.createComparableComparer
 import yfiles.algorithms.Graph
 import yfiles.layout.DefaultLayoutGraph
+import yfiles.layout.TabularLayout
 import yfiles.tree.NodeOrderComparer
 
 fun comparer() {
     val g: Graph = DefaultLayoutGraph()
     g.sortNodes(NodeOrderComparer())
     g.sortNodes(createComparableComparer())
+
+    val l = TabularLayout()
+    l.nodeComparer = NodeOrderComparer()
+    l.nodeComparer = createComparableComparer()
 }
