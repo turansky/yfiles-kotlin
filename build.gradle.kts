bce99130523b147cffdf404f1b211df2c0df3c52
Victor Turansky <victor.turansky@gmail.com>
2019-11-02 13:05:40 +0300

    Gradle 5.6.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,6 @@ subprojects {
 }
 
 tasks.wrapper {
-    gradleVersion = "5.6.3"
+    gradleVersion = "5.6.4"
     distributionType = Wrapper.DistributionType.ALL
 }
--- plugins/yfiles/build.gradle.kts
+++ plugins/yfiles/build.gradle.kts
@@ -78,6 +78,6 @@ pluginBundle {
 }
 
 tasks.wrapper {
-    gradleVersion = "5.6.3"
+    gradleVersion = "5.6.4"
     distributionType = Wrapper.DistributionType.ALL
 }
