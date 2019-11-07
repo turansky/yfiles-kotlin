db5ac6e44159c39b4dac3c8f22a7a6321fc0dcf1
Victor Turansky <victor.turansky@gmail.com>
2019-11-07 23:44:29 +0300

    args -> event

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/EventHacks.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/EventHacks.kt
@@ -1,5 +1,7 @@
 package com.github.turansky.yfiles.correction
 
+import org.json.JSONObject
+
 internal fun applyEventHacks(source: Source) {
     source.types()
         .flatMap { it.optJsequence(J_CONSTRUCTORS) + it.optJsequence(J_METHODS) }
@@ -7,12 +9,23 @@ internal fun applyEventHacks(source: Source) {
         .filter { it.has(J_PARAMETERS) }
         .jsequence(J_PARAMETERS)
         .forEach {
-            val name = when (it.getString(J_NAME)) {
-                "evt" -> "event"
-                "src", "eventSource" -> "source"
-                else -> return@forEach
-            }
+            val name = it.getNewName()
+                ?: return@forEach
 
             it.put(J_NAME, name)
         }
 }
+
+fun JSONObject.getNewName(): String? =
+    when (getString(J_NAME)) {
+        "evt" -> "event"
+        "args" -> if (getString(J_TYPE).endsWith("Args")) {
+            "event"
+        } else {
+            null
+        }
+
+        "src", "eventSource" -> "source"
+
+        else -> null
+    }
