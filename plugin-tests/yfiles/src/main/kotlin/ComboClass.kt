821905195a175843e0fdb97d91a74266d41b81ae
Victor Turansky <victor.turansky@gmail.com>
2019-11-20 22:38:05 +0300

    Plugin. Add object test example

--- plugin-tests/yfiles/src/main/kotlin/ComboClass.kt
+++ plugin-tests/yfiles/src/main/kotlin/ComboClass.kt
@@ -1,8 +1,16 @@
+import yfiles.geometry.Rect
 import yfiles.view.IBoundsProvider
+import yfiles.view.ICanvasContext
 import yfiles.view.IVisibilityTestable
 
 abstract class ComboClass : IVisibilityTestable, IBoundsProvider
 
 abstract class SuperComboClass : ComboClass() {
     val puper = true
+}
+
+object SimpleComboObject : IVisibilityTestable, IBoundsProvider {
+    override fun isVisible(context: ICanvasContext, rectangle: Rect): Boolean = false
+
+    override fun getBounds(context: ICanvasContext): Rect = TODO()
 }
