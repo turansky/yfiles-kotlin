91a7172135ce3975b66c23f09fc9cd21312c6b2d
Victor Turansky <victor.turansky@gmail.com>
2019-11-20 11:31:02 +0300

    IDEA plugin. Add Kotlin plugin

--- ide-plugins/yfiles/build.gradle.kts
+++ ide-plugins/yfiles/build.gradle.kts
@@ -1,8 +1,16 @@
 group = "com.github.turansky.yfiles"
 version = "0.0.1-SNAPSHOT"
 
+plugins {
+    kotlin("jvm") version "1.3.60"
+}
+
 repositories {
     jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
 }
 
 tasks.wrapper {
