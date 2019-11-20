abe291f230fff9c07743cb41edb67b10e33dbe66
Victor Turansky <victor.turansky@gmail.com>
2019-11-20 11:06:53 +0300

    IDEA plugin. Configure repositories

--- ide-plugins/yfiles/build.gradle.kts
+++ ide-plugins/yfiles/build.gradle.kts
@@ -1,6 +1,10 @@
 group = "com.github.turansky.yfiles"
 version = "0.0.1-SNAPSHOT"
 
+repositories {
+    jcenter()
+}
+
 tasks.wrapper {
     gradleVersion = "6.0.1"
     distributionType = Wrapper.DistributionType.ALL
--- ide-plugins/yfiles/settings.gradle.kts
+++ ide-plugins/yfiles/settings.gradle.kts
@@ -1,1 +1,8 @@
 rootProject.name = "idea-plugin"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        jcenter()
+    }
+}
