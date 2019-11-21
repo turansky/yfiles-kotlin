a4b259738deff3f50901147f51165270cf2f890c
Victor Turansky <victor.turansky@gmail.com>
2019-11-21 23:09:16 +0300

    Plugin. Fix import

--- plugins/yfiles/src/main/kotlin/com/github/turansky/yfiles/compiler/backend/ir/IrExtension.kt
+++ plugins/yfiles/src/main/kotlin/com/github/turansky/yfiles/compiler/backend/ir/IrExtension.kt
@@ -2,11 +2,12 @@ package com.github.turansky.yfiles.compiler.backend.ir
 
 import org.jetbrains.kotlin.backend.common.BackendContext
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
+import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.resolve.BindingContext
 
 internal class IrExtension : IrGenerationExtension {
     override fun generate(
-        file: org.jetbrains.kotlin.ir.declarations.IrFile,
+        file: IrFile,
         backendContext: BackendContext,
         bindingContext: BindingContext
     ) {
