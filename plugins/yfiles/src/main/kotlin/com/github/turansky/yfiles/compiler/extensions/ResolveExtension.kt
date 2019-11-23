3ae22fe92f09cf4e3b8d659e0da35ffb06925213
Victor Turansky <victor.turansky@gmail.com>
2019-11-23 12:10:17 +0300

    Plugin. Empty SyntheticResolveExtension

---
+++ plugins/yfiles/src/main/kotlin/com/github/turansky/yfiles/compiler/extensions/ResolveExtension.kt
@@ -0,0 +1,5 @@
+package com.github.turansky.yfiles.compiler.extensions
+
+import org.jetbrains.kotlin.resolve.extensions.SyntheticResolveExtension
+
+internal class ResolveExtension : SyntheticResolveExtension
