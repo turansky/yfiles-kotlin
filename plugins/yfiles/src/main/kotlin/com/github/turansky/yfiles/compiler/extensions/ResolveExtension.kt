fd8f46ed85162442e6743c38304fd26cd414ce47
Victor Turansky <victor.turansky@gmail.com>
2019-11-23 12:39:12 +0300

    Plugin. Add synthetic companion for YObject direct inheritors

--- plugins/yfiles/src/main/kotlin/com/github/turansky/yfiles/compiler/extensions/ResolveExtension.kt
+++ plugins/yfiles/src/main/kotlin/com/github/turansky/yfiles/compiler/extensions/ResolveExtension.kt
@@ -1,5 +1,20 @@
 package com.github.turansky.yfiles.compiler.extensions
 
+import com.github.turansky.yfiles.compiler.backend.common.extendsYObject
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.descriptors.ClassKind
+import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.name.SpecialNames.DEFAULT_NAME_FOR_COMPANION_OBJECT
 import org.jetbrains.kotlin.resolve.extensions.SyntheticResolveExtension
 
-internal class ResolveExtension : SyntheticResolveExtension
+internal val ClassDescriptor.shouldHaveGeneratedCompanion: Boolean
+    get() = kind == ClassKind.CLASS && extendsYObject
+
+internal class ResolveExtension : SyntheticResolveExtension {
+    override fun getSyntheticCompanionObjectNameIfNeeded(thisDescriptor: ClassDescriptor): Name? =
+        if (thisDescriptor.shouldHaveGeneratedCompanion) {
+            DEFAULT_NAME_FOR_COMPANION_OBJECT
+        } else {
+            null
+        }
+}
