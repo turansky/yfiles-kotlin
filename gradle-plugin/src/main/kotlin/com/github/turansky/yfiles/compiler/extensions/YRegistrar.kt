8cc5d197b2746f6b978fbadf705442723d7a5c6b
Victor Turansky <victor.turansky@gmail.com>
2019-11-24 11:04:15 +0300

    Plugin. Temp ResolveExtension excluding

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/extensions/YRegistrar.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/extensions/YRegistrar.kt
@@ -7,15 +7,12 @@ import org.jetbrains.kotlin.com.intellij.mock.MockProject
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.js.translate.extensions.JsSyntheticTranslateExtension
-import org.jetbrains.kotlin.resolve.extensions.SyntheticResolveExtension
 
 class YRegistrar : ComponentRegistrar {
     override fun registerProjectComponents(
         project: MockProject,
         configuration: CompilerConfiguration
     ) {
-        SyntheticResolveExtension.registerExtension(project, ResolveExtension())
-
         IrGenerationExtension.registerExtension(project, IrLoweringExtension())
         JsSyntheticTranslateExtension.registerExtension(project, JsExtension())
     }
