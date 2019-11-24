5750dbae019e9b2f60edf67a8c33c77a95ce84b2
Victor Turansky <victor.turansky@gmail.com>
2019-11-24 20:33:42 +0300

    IDEA Plugin test. Add test class

---
+++ idea-plugin-test/src/main/kotlin/CustomObject.kt
@@ -0,0 +1,17 @@
+import yfiles.lang.ClassMetadata
+import yfiles.lang.YObject
+import kotlin.browser.window
+
+class CustomObject : YObject() {
+    fun hallo() {
+        window.alert("Hallo from CustomObject!")
+    }
+
+    companion object : ClassMetadata<CustomObject>
+}
+
+class OtherCustomObject : YObject() {
+    fun hallo() {
+        window.alert("Hallo from OtherCustomObject!")
+    }
+}
