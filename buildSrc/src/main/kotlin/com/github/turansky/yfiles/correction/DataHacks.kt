d32262e53ce92717987b29c61c5a386ff252a96d
Victor Turansky <victor.turansky@gmail.com>
2019-11-27 02:26:36 +0300

    DataMaps. Add simple type parameters

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/DataHacks.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/DataHacks.kt
@@ -1,6 +1,7 @@
 package com.github.turansky.yfiles.correction
 
 import com.github.turansky.yfiles.JS_OBJECT
+import org.json.JSONObject
 
 internal fun applyDataHacks(source: Source) {
     fixClass(source)
@@ -16,4 +17,21 @@ private fun fixClass(source: Source) {
         source.type(it.substringAfterLast("."))
             .setSingleTypeParameter("V", JS_OBJECT)
     }
+
+    source.types()
+        .flatMap { it.getTypeHolders() }
+        .filter { it.getString(J_TYPE) in MAP_INTERFACES }
+        .forEach { it.put(J_TYPE, it.getString(J_TYPE) + "<*>") }
 }
+
+fun JSONObject.getTypeHolders() =
+    (optJsequence(J_STATIC_METHODS) + optJsequence(J_METHODS))
+        .flatMap { it.optJsequence(J_PARAMETERS) + it.returnsSequence() }
+        .plus(optJsequence(J_PROPERTIES))
+
+private fun JSONObject.returnsSequence(): Sequence<JSONObject> =
+    if (has(J_RETURNS)) {
+        sequenceOf(getJSONObject(J_RETURNS))
+    } else {
+        emptySequence()
+    }
