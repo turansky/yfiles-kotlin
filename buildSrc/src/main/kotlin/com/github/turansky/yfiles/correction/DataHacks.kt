7b1c242f56d1d7d7bc6e488a2dca74fc9316da97
Victor Turansky <victor.turansky@gmail.com>
2019-11-27 02:36:21 +0300

    Apply *DataMap hacks

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/DataHacks.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/DataHacks.kt
@@ -4,7 +4,7 @@ import com.github.turansky.yfiles.JS_OBJECT
 import org.json.JSONObject
 
 internal fun applyDataHacks(source: Source) {
-    fixClass(source)
+    fixDataMaps(source)
 }
 
 private val MAP_INTERFACES = setOf(
@@ -12,7 +12,7 @@ private val MAP_INTERFACES = setOf(
     "yfiles.algorithms.INodeMap"
 )
 
-private fun fixClass(source: Source) {
+private fun fixDataMaps(source: Source) {
     MAP_INTERFACES.forEach {
         source.type(it.substringAfterLast("."))
             .setSingleTypeParameter("V", JS_OBJECT)
@@ -22,6 +22,14 @@ private fun fixClass(source: Source) {
         .flatMap { it.getTypeHolders() }
         .filter { it.getString(J_TYPE) in MAP_INTERFACES }
         .forEach { it.put(J_TYPE, it.getString(J_TYPE) + "<*>") }
+
+    source.type("Graph")
+        .jsequence(J_PROPERTIES)
+        .forEach { property ->
+            val type = property.getString(J_TYPE)
+            MAP_INTERFACES.find { it in type }
+                ?.also { property.put(J_TYPE, type.replace(it, "$it<*>")) }
+        }
 }
 
 fun JSONObject.getTypeHolders() =
--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/Hacks.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/Hacks.kt
@@ -49,6 +49,7 @@ internal fun applyHacks(api: JSONObject) {
     applyComparableHacks(source)
     applyComparerHacks(source)
     applyCursorHacks(source)
+    applyDataHacks(source)
     applyDpKeyHacks(source)
     applyListHacks(source)
     applyYListHacks(source)
