8f52ff56bc50c1def12c99b9505b1bf701b1e77a
Victor Turansky <victor.turansky@gmail.com>
2019-12-01 20:53:16 +0300

    Use PartitionCellKey

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/PartitionCellHacks.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/PartitionCellHacks.kt
@@ -1,5 +1,7 @@
 package com.github.turansky.yfiles.correction
 
+import com.github.turansky.yfiles.JS_OBJECT
+import com.github.turansky.yfiles.PARTITION_CELL_KEY
 import java.io.File
 
 internal fun generatePartitionCellUtils(sourceDir: File) {
@@ -17,7 +19,25 @@ internal fun generatePartitionCellUtils(sourceDir: File) {
         )
 }
 
-@Suppress("UNUSED_PARAMETER")
 internal fun applyPartitionCellHacks(source: Source) {
-    // implement
+    val methodNames = setOf(
+        "getData",
+        "putData",
+        "removeData"
+    )
+
+    source.type("PartitionCell")
+        .flatMap(METHODS)
+        .filter { it[NAME] in methodNames }
+        .onEach { it.setSingleTypeParameter(bound = JS_OBJECT) }
+        .onEach { it[RETURNS][TYPE] = "T" }
+        .onEach { it.changeNullability(true) }
+        .flatMap(PARAMETERS)
+        .forEach {
+            it[TYPE] = when (val name = it[NAME]) {
+                "key" -> "$PARTITION_CELL_KEY<T>"
+                "data" -> "T"
+                else -> throw IllegalArgumentException("Unable to calculate type by name '$name'")
+            }
+        }
 }
