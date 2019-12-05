a6c95c7e0fd56afbdcddfd27504c5f9551532b4a
Victor Turansky <victor.turansky@gmail.com>
2019-12-05 03:15:05 +0300

    ILabelModelParameter instead of Any #2

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/LabelModelParameterHacks.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/LabelModelParameterHacks.kt
@@ -12,6 +12,21 @@ internal fun applyLabelModelParameterHacks(source: Source) {
     ).forEach { it[PROPERTIES]["modelParameter"][TYPE] = ILABEL_MODEL_PARAMETER }
 
     source.types(
+        "LabelCandidate",
+
+        "EdgeLabelCandidate",
+        "ExtendedEdgeLabelCandidate",
+
+        "NodeLabelCandidate",
+        "ExtendedNodeLabelCandidate",
+
+        "LayoutGraphUtilities"
+    ).flatMap { it.optFlatMap(CONSTRUCTORS) + it.optFlatMap(METHODS) + it.optFlatMap(STATIC_METHODS) }
+        .optFlatMap(PARAMETERS)
+        .filter { it[NAME] == "param" }
+        .forEach { it[TYPE] = ILABEL_MODEL_PARAMETER }
+
+    source.types(
         "IEdgeLabelLayoutModel",
         "DiscreteEdgeLabelLayoutModel",
         "FreeEdgeLabelLayoutModel",
