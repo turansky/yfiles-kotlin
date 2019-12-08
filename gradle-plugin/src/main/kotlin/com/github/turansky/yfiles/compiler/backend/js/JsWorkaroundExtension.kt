c9b2b10d9275ff0d0e0fef8cbeeb49ab4c09d731
Victor Turansky <victor.turansky@gmail.com>
2019-12-09 00:55:27 +0300

    Plugin. Call workaround apply for KT-34770

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/backend/js/JsWorkaroundExtension.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/backend/js/JsWorkaroundExtension.kt
@@ -3,11 +3,22 @@ package com.github.turansky.yfiles.compiler.backend.js
 import com.github.turansky.yfiles.compiler.backend.common.LANG_PACKAGE
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.descriptors.ClassKind.CLASS
+import org.jetbrains.kotlin.descriptors.findClassAcrossModuleDependencies
+import org.jetbrains.kotlin.js.backend.ast.JsExpression
+import org.jetbrains.kotlin.js.backend.ast.JsInvocation
+import org.jetbrains.kotlin.js.backend.ast.JsStringLiteral
 import org.jetbrains.kotlin.js.translate.context.TranslationContext
 import org.jetbrains.kotlin.js.translate.declaration.DeclarationBodyVisitor
 import org.jetbrains.kotlin.js.translate.extensions.JsSyntheticTranslateExtension
+import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.Name.identifier
 import org.jetbrains.kotlin.psi.KtPureClassOrObject
+import org.jetbrains.kotlin.resolve.DescriptorUtils.getFunctionByName
+
+private val KOTLIN_WORKAROUNDS = ClassId(LANG_PACKAGE, identifier("KotlinWorkarounds"))
+private val APPLY = identifier("apply")
+
+private const val KT_34770 = "KT-34770"
 
 // TODO: remove after fix - https://youtrack.jetbrains.com/issue/KT-34770
 class JsWorkaroundExtension : JsSyntheticTranslateExtension {
@@ -26,5 +37,28 @@ class JsWorkaroundExtension : JsSyntheticTranslateExtension {
             descriptor.isData -> return
             !descriptor.annotations.hasAnnotation(CONFIGURABLE_PROPERTIES) -> return
         }
+
+        val classId = descriptor.name.identifier
+        context.apply {
+            declareConstantValue(
+                suggestedName = generateName(classId, "propertiesConfigured"),
+                value = fixProperties(descriptor)
+            ).also { addDeclarationStatement(it.makeStmt()) }
+        }
     }
+}
+
+private fun TranslationContext.fixProperties(
+    descriptor: ClassDescriptor
+): JsExpression {
+    val applyWorkaround = currentModule
+        .findClassAcrossModuleDependencies(KOTLIN_WORKAROUNDS)!!
+        .unsubstitutedMemberScope
+        .let { getFunctionByName(it, APPLY) }
+
+    return JsInvocation(
+        toValueReference(applyWorkaround),
+        toValueReference(descriptor),
+        JsStringLiteral(KT_34770)
+    )
 }
