3bfbe16c2f885cfc171410d5b073893b269dcdab
Victor Turansky <victor.turansky@gmail.com>
2019-12-09 00:55:53 +0300

    Update configurable properties examples

--- examples/configurable-properties/src/main/kotlin/CustomLabelModelParameter.kt
+++ examples/configurable-properties/src/main/kotlin/CustomLabelModelParameter.kt
@@ -2,7 +2,9 @@ import yfiles.graph.FreeEdgeLabelModel
 import yfiles.graph.ILabel
 import yfiles.graph.ILabelModel
 import yfiles.graph.ILabelModelParameter
+import yfiles.lang.ConfigurableProperties
 
+@ConfigurableProperties
 class CustomLabelModelParameter : ILabelModelParameter {
     override val model: ILabelModel
         get() = FreeEdgeLabelModel.INSTANCE
