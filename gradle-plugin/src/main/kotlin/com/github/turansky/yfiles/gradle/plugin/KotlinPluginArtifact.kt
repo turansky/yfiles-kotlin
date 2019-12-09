ebb79eb1117b8757a8c33eb30991b79432bb7867
Victor Turansky <victor.turansky@gmail.com>
2019-12-10 01:34:18 +0300

    Plugin. Prepare release

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/KotlinPluginArtifact.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/KotlinPluginArtifact.kt
@@ -6,5 +6,5 @@ internal val KOTLIN_PLUGIN_ARTIFACT: SubpluginArtifact
     get() = SubpluginArtifact(
         groupId = "gradle.plugin.com.github.turansky.yfiles",
         artifactId = "gradle-plugin",
-        version = "0.6.1-SNAPSHOT"
+        version = "0.7.0-SNAPSHOT"
     )
