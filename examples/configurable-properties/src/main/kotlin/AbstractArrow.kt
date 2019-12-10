4a7724d5bf4f5c302663880b2d9da7f7784e5d95
Victor Turansky <victor.turansky@gmail.com>
2019-12-10 03:26:24 +0300

    Simplify AbstractArrow

--- examples/configurable-properties/src/main/kotlin/AbstractArrow.kt
+++ examples/configurable-properties/src/main/kotlin/AbstractArrow.kt
@@ -3,9 +3,6 @@ import yfiles.styles.IArrow
 
 @ConfigurableProperties
 abstract class AbstractArrow : IArrow {
-    override val cropLength: Double
-        get() = 13.0
-
-    override val length: Double
-        get() = 42.0
+    override val cropLength = 13.0
+    override val length = 42.0
 }
