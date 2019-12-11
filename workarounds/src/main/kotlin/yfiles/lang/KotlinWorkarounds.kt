7fe429ed100f34086712de9bc9b4b6e55680a3cf
Victor Turansky <victor.turansky@gmail.com>
2019-12-11 13:18:21 +0300

    Update KotlinWorkarounds. Update Object in scope

--- workarounds/src/main/kotlin/yfiles/lang/KotlinWorkarounds.kt
+++ workarounds/src/main/kotlin/yfiles/lang/KotlinWorkarounds.kt
@@ -2,6 +2,8 @@
 
 package yfiles.lang
 
+import kotlin.browser.window
+
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.BINARY)
 annotation class ConfigurableProperties
@@ -9,43 +11,33 @@ annotation class ConfigurableProperties
 // TODO: remove after fix - https://youtrack.jetbrains.com/issue/KT-34770
 @Deprecated(message = "For code generation only", level = DeprecationLevel.HIDDEN)
 object KotlinWorkarounds {
-    fun apply(
-        jsClass: JsClass<*>,
-        workaroundName: String
-    ) {
-        if (workaroundName != "KT-34770") {
+    fun apply(scope: dynamic) {
+        val localScope = scope.unsafeCast<Scope>()
+        if (localScope.Object != null) {
             return
         }
 
-        jsClass.kt34770 = true
-        if (Object.defineProperty.kt34770 == true) {
-            return
+        val globalObject = window.unsafeCast<Scope>().Object!!
+
+        val localObject: Object = js("{}")
+        localObject.defineProperty = { obj, prop, descriptor ->
+            descriptor.configurable = true
+
+            globalObject.defineProperty(obj, prop, descriptor)
         }
 
-        val originalDefineProperty = Object.defineProperty
-        Object.defineProperty = { obj, prop, descriptor ->
-            if (obj.constructor?.kt34770 == true) {
-                descriptor.configurable = true
-            }
-
-            originalDefineProperty(obj, prop, descriptor)
-        }
+        localScope.Object = localObject
     }
 }
 
-private external object Object {
+private external interface Scope {
+    var Object: Object?
+}
+
+private external interface Object {
     var defineProperty: (obj: Any, prop: String, descriptor: ObjectPropertyDescriptor) -> Unit
 }
 
 private external interface ObjectPropertyDescriptor {
     var configurable: Boolean?
 }
-
-private inline val Any.constructor: Any?
-    get() = asDynamic().constructor
-
-private inline var Any.kt34770: Boolean?
-    get() = asDynamic().kt34770
-    set(value) {
-        asDynamic().kt34770 = value
-    }
