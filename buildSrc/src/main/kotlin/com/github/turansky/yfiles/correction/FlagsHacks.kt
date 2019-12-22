1e96979b24e15477b48d108eea0fe14dc092c3ae
Victor Turansky <victor.turansky@gmail.com>
2019-12-22 15:09:30 +0300

    Update "Flags or Flags"

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/FlagsHacks.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/FlagsHacks.kt
@@ -1,6 +1,5 @@
 package com.github.turansky.yfiles.correction
 
-import com.github.turansky.yfiles.YENUM
 import java.io.File
 
 internal fun generateFlagsUtils(sourceDir: File) {
@@ -8,18 +7,20 @@ internal fun generateFlagsUtils(sourceDir: File) {
         .writeText(
             // language=kotlin
             """
+                |@file:Suppress("NOTHING_TO_INLINE")
+                |
                 |package yfiles.lang
                 |
                 |external interface Flags<T>
-                |   where T : Flags<T>,
-                |         T : $YENUM<T>
-                |         
-                |inline infix fun <T> Flags<T>.or(other: Flags<T>): Flags<T>
-                |   where T : Flags<T>,
-                |         T : yfiles.lang.YEnum<T> {
-                |   return unsafeCast<Int>()
-                |       .or(other.unsafeCast<Int>())
-                |       .unsafeCast<T>()
+                |        where T : Flags<T>,
+                |              T : YEnum<T>
+                |
+                |inline infix fun <T> T.or(other: T): T
+                |        where T : Flags<T>,
+                |              T : YEnum<T> {
+                |    return unsafeCast<Int>()
+                |        .or(other.unsafeCast<Int>())
+                |        .unsafeCast<T>()
                 |}
             """.trimMargin()
         )
