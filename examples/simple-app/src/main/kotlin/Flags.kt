38bfd55577e5941639374c441833bbafb65083d0
Victor Turansky <victor.turansky@gmail.com>
2019-12-23 02:59:10 +0300

    Add Flags.contains example

--- examples/simple-app/src/main/kotlin/Flags.kt
+++ examples/simple-app/src/main/kotlin/Flags.kt
@@ -2,10 +2,14 @@
 
 import yfiles.graph.GraphItemTypes.*
 import yfiles.input.GraphViewerInputMode
+import yfiles.lang.contains
 import yfiles.lang.or
 
 fun flags() {
-    GraphViewerInputMode {
+    val mode = GraphViewerInputMode {
         clickableItems = NODE or EDGE or LABEL
     }
+
+    println(NODE in mode.clickableItems)
+    println(EDGE in mode.clickableItems)
 }
