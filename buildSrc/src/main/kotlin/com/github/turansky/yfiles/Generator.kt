e4d23a31511c0fd883af569f826cd8b259f9094d
Victor Turansky <victor.turansky@gmail.com>
2019-12-27 01:11:09 +0300

    Separate JsonReader

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/Generator.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/Generator.kt
@@ -5,34 +5,7 @@ import com.github.turansky.yfiles.vsdx.correction.applyVsdxHacks
 import com.github.turansky.yfiles.vsdx.correction.correctVsdxNumbers
 import com.github.turansky.yfiles.vsdx.correction.createVsdxDataClasses
 import com.github.turansky.yfiles.vsdx.fakeVsdxInterfaces
-import org.json.JSONObject
 import java.io.File
-
-private fun readJson(
-    file: File,
-    action: JSONObject.() -> Unit
-): JSONObject =
-    file.readText(DEFAULT_CHARSET)
-        .run { substring(indexOf("{")) }
-        .run { JSONObject(this) }
-        .run { toString() }
-        .run { fixSystemPackage() }
-        .run { fixClassDeclaration() }
-        .run { JSONObject(this) }
-        .apply(action)
-        .run { toString() }
-        .run { JSONObject(this) }
-
-private fun String.fixSystemPackage(): String =
-    replace("\"yfiles.system.", "\"yfiles.lang.")
-        .replace("\"system.", "\"yfiles.lang.")
-
-private fun String.fixClassDeclaration(): String =
-    replace(""""id":"yfiles.lang.Class"""", """"id":"$YCLASS","es6name":"Class"""")
-        .replace(""""name":"Class"""", """"name":"YClass"""")
-        .replace(""""yfiles.lang.Class"""", """"$YCLASS"""")
-        .replace(""""Array<yfiles.lang.Class>"""", """"Array<$YCLASS>"""")
-        .replace(""""yfiles.collections.Map<yfiles.lang.Class,$JS_OBJECT>"""", """"yfiles.collections.Map<$YCLASS,$JS_OBJECT>"""")
 
 fun generateKotlinDeclarations(
     apiFile: File,
---
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/JsonReader.kt
@@ -0,0 +1,30 @@
+package com.github.turansky.yfiles
+
+import org.json.JSONObject
+import java.io.File
+
+internal fun readJson(
+    file: File,
+    action: JSONObject.() -> Unit
+): JSONObject =
+    file.readText(DEFAULT_CHARSET)
+        .run { substring(indexOf("{")) }
+        .run { JSONObject(this) }
+        .run { toString() }
+        .run { fixSystemPackage() }
+        .run { fixClassDeclaration() }
+        .run { JSONObject(this) }
+        .apply(action)
+        .run { toString() }
+        .run { JSONObject(this) }
+
+private fun String.fixSystemPackage(): String =
+    replace("\"yfiles.system.", "\"yfiles.lang.")
+        .replace("\"system.", "\"yfiles.lang.")
+
+private fun String.fixClassDeclaration(): String =
+    replace(""""id":"yfiles.lang.Class"""", """"id":"$YCLASS","es6name":"Class"""")
+        .replace(""""name":"Class"""", """"name":"YClass"""")
+        .replace(""""yfiles.lang.Class"""", """"$YCLASS"""")
+        .replace(""""Array<yfiles.lang.Class>"""", """"Array<$YCLASS>"""")
+        .replace(""""yfiles.collections.Map<yfiles.lang.Class,$JS_OBJECT>"""", """"yfiles.collections.Map<$YCLASS,$JS_OBJECT>"""")
