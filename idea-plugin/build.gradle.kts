5e459c09f5998002698faa2af3a44d4edd08d930
Victor Turansky <victor.turansky@gmail.com>
2019-12-28 23:19:02 +0300

    IDEA Plugin. Use "autostyle"

--- idea-plugin/build.gradle.kts
+++ idea-plugin/build.gradle.kts
@@ -4,6 +4,7 @@ version = "0.0.5-SNAPSHOT"
 plugins {
     kotlin("jvm") version "1.3.61"
     id("org.jetbrains.intellij") version "0.4.15"
+    id("com.github.autostyle") version "3.0"
 }
 
 repositories {
@@ -20,6 +21,12 @@ intellij {
     version = "2019.3.1"
 
     setPlugins("org.jetbrains.kotlin:1.3.61-release-IJ2019.3-1")
+}
+
+autostyle {
+    kotlin {
+        endWithNewline()
+    }
 }
 
 tasks {
