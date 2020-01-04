344aad59dee994f69deb09561b695b6452c5b48b
Victor Turansky <victor.turansky@gmail.com>
2020-01-04 19:15:09 +0300

    IDEA plugin. Empty InheritanceInspection

--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/InheritanceInspection.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/InheritanceInspection.kt
@@ -4,6 +4,16 @@ import com.intellij.codeInspection.InspectionManager
 import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
+import org.jetbrains.kotlin.idea.project.platform
+import org.jetbrains.kotlin.lexer.KtTokens.EXTERNAL_KEYWORD
+import org.jetbrains.kotlin.platform.js.isJs
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtModifierListOwner
+import org.jetbrains.kotlin.psi.KtSuperTypeList
+import org.jetbrains.kotlin.psi.psiUtil.anyDescendantOfType
+import org.jetbrains.kotlin.psi.psiUtil.getChildrenOfType
+
+private const val YOBJECT = "yfiles.lang.YObject"
 
 class InheritanceInspection : AbstractKotlinInspection() {
     override fun checkFile(
@@ -11,6 +21,43 @@ class InheritanceInspection : AbstractKotlinInspection() {
         manager: InspectionManager,
         isOnTheFly: Boolean
     ): Array<ProblemDescriptor>? {
-        return ProblemDescriptor.EMPTY_ARRAY
+        if (!manager.project.platform.isJs()) {
+            return ProblemDescriptor.EMPTY_ARRAY
+        }
+
+        val problems = file.getChildrenOfType<KtClass>()
+            .mapNotNull { checkClass(it) }
+
+        if (problems.isEmpty()) {
+            return ProblemDescriptor.EMPTY_ARRAY
+        }
+
+        return problems.toTypedArray()
+    }
+
+    private fun checkClass(declaration: KtClass): ProblemDescriptor? {
+        if (declaration.isExternal()) {
+            return null
+        }
+
+        val superTypes = declaration.getSuperTypeList()
+            ?.takeIf { it.implementsExternalInterfaceDirectly() }
+            ?: return null
+
+        if (superTypes.implementsYObject()) {
+            return null
+        }
+
+        return null
     }
 }
+
+private fun KtModifierListOwner.isExternal(): Boolean =
+    hasModifier(EXTERNAL_KEYWORD)
+
+private fun KtSuperTypeList.implementsExternalInterfaceDirectly(): Boolean =
+    getChildrenOfType<KtClass>()
+        .any { it.isExternal() && it.isInterface() }
+
+private fun KtSuperTypeList.implementsYObject(): Boolean =
+    anyDescendantOfType<KtClass> { it.name == YOBJECT }
