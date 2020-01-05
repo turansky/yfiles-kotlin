6fa0cd46d210a4c1deacbe3a3205dd117e8f53f1
Victor Turansky <victor.turansky@gmail.com>
2020-01-06 02:02:03 +0300

    IDEA plugin. Simple check

--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/InheritanceInspection.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/InheritanceInspection.kt
@@ -3,16 +3,14 @@ package com.github.turansky.yfiles.ide
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
 import org.jetbrains.kotlin.idea.project.platform
-import org.jetbrains.kotlin.lexer.KtTokens.EXTERNAL_KEYWORD
-import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.idea.search.usagesSearch.descriptor
 import org.jetbrains.kotlin.platform.js.isJs
-import org.jetbrains.kotlin.psi.*
-import org.jetbrains.kotlin.psi.psiUtil.anyDescendantOfType
-import org.jetbrains.kotlin.psi.psiUtil.getChildrenOfType
-
-private val YOBJECT = FqName("yfiles.lang.YObject")
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtVisitorVoid
 
 class InheritanceInspection : AbstractKotlinInspection() {
     override fun buildVisitor(
@@ -27,17 +25,18 @@ class InheritanceInspection : AbstractKotlinInspection() {
             }
 
             override fun visitClass(klass: KtClass) {
-                if (klass.isExternal()) {
+                val descriptor = klass.descriptor as? ClassDescriptor
+                    ?: return
+
+                if (descriptor.isExternal) {
                     return
                 }
 
-                val superTypes = klass.getSuperTypeList()
-                    ?.takeIf { it.implementsExternalInterfaceDirectly() }
-                    ?.takeIf { it.implementsYObject() }
+                val superTypeList = klass.getSuperTypeList()
                     ?: return
 
                 holder.registerProblem(
-                    superTypes,
+                    superTypeList,
                     "YObject inheritor detected!",
                     ProblemHighlightType.GENERIC_ERROR
                 )
@@ -45,13 +44,3 @@ class InheritanceInspection : AbstractKotlinInspection() {
         }
     }
 }
-
-private fun KtModifierListOwner.isExternal(): Boolean =
-    hasModifier(EXTERNAL_KEYWORD)
-
-private fun KtSuperTypeList.implementsExternalInterfaceDirectly(): Boolean =
-    getChildrenOfType<KtClass>()
-        .any { it.isExternal() && it.isInterface() }
-
-private fun KtSuperTypeList.implementsYObject(): Boolean =
-    anyDescendantOfType<KtClass> { it.fqName == YOBJECT }
