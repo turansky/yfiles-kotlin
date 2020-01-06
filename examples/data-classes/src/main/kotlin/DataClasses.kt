c9daf36cf999fb9f3f3dc5efb5314ff2c1235dd3
Victor Turansky <victor.turansky@gmail.com>
2020-01-07 02:26:54 +0300

    Add second test data class

--- examples/data-classes/src/main/kotlin/DataClasses.kt
+++ examples/data-classes/src/main/kotlin/DataClasses.kt
@@ -1,6 +1,15 @@
+import yfiles.lang.IComparable
 import yfiles.lang.YObject
 
 data class Person(
     val firstName: String,
     val lastName: String
 ) : YObject
+
+data class YDate(
+    val milliseconds: Int
+) : IComparable<YDate> {
+    override fun compareTo(other: YDate): Int {
+        return milliseconds.compareTo(other.milliseconds)
+    }
+}
