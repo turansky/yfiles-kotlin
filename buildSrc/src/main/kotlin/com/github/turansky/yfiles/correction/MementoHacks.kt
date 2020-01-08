17b5e628ed885eff27961d3c2634b7f015562cdc
Victor Turansky <victor.turansky@gmail.com>
2020-01-08 16:11:36 +0300

    Hierarchic mementos #2

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/MementoHacks.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/MementoHacks.kt
@@ -1,9 +1,11 @@
 package com.github.turansky.yfiles.correction
 
 import com.github.turansky.yfiles.GeneratorContext
+import com.github.turansky.yfiles.JS_ANY
+import com.github.turansky.yfiles.json.get
 
-private const val LAYER_CONSTRAINTS_MEMENTO = "yfiles.hierarchic.LayerConstraintsMemento"
-private const val SEQUENCE_CONSTRAINTS_MEMENTO = "yfiles.hierarchic.SequenceConstraintsMemento"
+internal const val LAYER_CONSTRAINTS_MEMENTO = "yfiles.hierarchic.LayerConstraintsMemento"
+internal const val SEQUENCE_CONSTRAINTS_MEMENTO = "yfiles.hierarchic.SequenceConstraintsMemento"
 
 internal fun generateMementoUtils(context: GeneratorContext) {
     // language=kotlin
@@ -26,4 +28,17 @@ internal fun applyMementoHacks(source: Source) {
 
     source.type("ISequenceConstraintFactory")
         .property("memento")[TYPE] = SEQUENCE_CONSTRAINTS_MEMENTO
+
+    source.type("HierarchicLayout")
+        .get(CONSTANTS).apply {
+            sequenceOf(
+                "LAYER_CONSTRAINTS_MEMENTO_DP_KEY" to LAYER_CONSTRAINTS_MEMENTO,
+                "SEQUENCE_CONSTRAINTS_MEMENTO_DP_KEY" to SEQUENCE_CONSTRAINTS_MEMENTO
+            ).forEach { (name, typeParameter) ->
+                get(name).also {
+                    require(it[TYPE] == graphDpKey(JS_ANY))
+                    it[TYPE] = graphDpKey(typeParameter)
+                }
+            }
+        }
 }
