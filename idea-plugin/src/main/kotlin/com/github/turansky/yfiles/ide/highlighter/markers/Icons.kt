626830fa6ac0d806fc833d4ba1c46a73027674c2
Victor Turansky <victor.turansky@gmail.com>
2020-01-26 17:18:51 +0300

    IDEA plugin. Add icon 'gutter' folder

--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/highlighter/markers/Icons.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/highlighter/markers/Icons.kt
@@ -5,7 +5,7 @@ import javax.swing.Icon
 
 internal object Icons {
     object Gutter {
-        val BASE_CLASS: Icon = IconLoader.getIcon("/icons/plugin.svg")
-        val CLASS_FIX_TYPE: Icon = IconLoader.getIcon("/icons/plugin.svg")
+        val BASE_CLASS: Icon = IconLoader.getIcon("/icons/gutter/plugin.svg")
+        val CLASS_FIX_TYPE: Icon = IconLoader.getIcon("/icons/gutter/plugin.svg")
     }
 }
