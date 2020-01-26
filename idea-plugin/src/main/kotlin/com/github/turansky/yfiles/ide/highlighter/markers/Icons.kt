6daf138ae9cfc1c4a0f07bbc83f736e5ce4bec4c
Victor Turansky <victor.turansky@gmail.com>
2020-01-26 17:17:00 +0300

    IDEA plugin. Use standard plugin icons

--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/highlighter/markers/Icons.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/highlighter/markers/Icons.kt
@@ -5,7 +5,7 @@ import javax.swing.Icon
 
 internal object Icons {
     object Gutter {
-        val BASE_CLASS: Icon = IconLoader.getIcon("/icons/base-class.svg")
-        val CLASS_FIX_TYPE: Icon = IconLoader.getIcon("/icons/class-fix-type.svg")
+        val BASE_CLASS: Icon = IconLoader.getIcon("/icons/plugin.svg")
+        val CLASS_FIX_TYPE: Icon = IconLoader.getIcon("/icons/plugin.svg")
     }
 }
