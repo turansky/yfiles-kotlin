f30fc23607cf1c5e12fb5c844468bb8a9e33ed66
Victor Turansky <victor.turansky@gmail.com>
2020-01-29 01:11:33 +0300

    Disable sourcemaps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
 import org.jetbrains.kotlin.gradle.plugin.KotlinJsPluginWrapper
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
 
 plugins {
     kotlin("js") version "1.3.61" apply false
@@ -14,6 +15,10 @@ allprojects {
 
 subprojects {
     plugins.withType<KotlinJsPluginWrapper> {
+        tasks.withType<KotlinWebpack>().configureEach {
+            sourceMaps = false
+        }
+
         tasks.withType<KotlinJsCompile>().configureEach {
             kotlinOptions {
                 moduleKind = "commonjs"
