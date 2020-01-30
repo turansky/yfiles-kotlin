f451aedfe0b25571d458ff7e83a66d747d75e762
Victor Turansky <victor.turansky@gmail.com>
2020-01-30 03:08:13 +0300

    Add base resource keys

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/Resources.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/Resources.kt
@@ -7,17 +7,48 @@ internal fun generateResourceTypes(
     devguide: JSONObject,
     context: GeneratorContext
 ) {
-    val content = devguide
+    val keyDeclarations = devguide
         .flatMap(CHILDREN)
         .optFlatMap(CHILDREN)
         .optFlatMap(CHILDREN)
         .optFlatMap(CHILDREN)
         .first { it.opt(ID) == "tab_resource_defaults" }
         .get(CONTENT)
+        .between("<tbody ", "</tbody>")
+        .splitToSequence("</para>")
+        .map { it.substringAfterLast(">") }
+        .chunked(2)
+        .filter { it.size == 2 }
+        .map { keyDeclaration(it.first(), it.last()) }
+        .joinToString(separator = "\n\n", postfix = "\n")
 
-    context["yfiles.Resources"] = """
-        /*
-        $content
-        */
+    context["yfiles.ResourceKey"] = """
+        package yfiles
+        
+        @JsName("String")
+        external class ResourceKey
+        internal constructor()
+        
+        fun ResourceKey(source:String):ResourceKey = 
+            source.unsafeCast<ResourceKey>()
+        
+        @JsName("String")
+        external class Hotkey
+        internal constructor()
+        
+        fun Hotkey(source:String):Hotkey = 
+            source.unsafeCast<Hotkey>()
+        
+        $keyDeclarations
+    """.trimIndent()
+}
+
+private fun keyDeclaration(key: String, defaultValue: String): String {
+    val name = key.replace(".", "_")
+    return """
+        /**
+         * Default value - $defaultValue
+         */
+         val $name = "$key"
     """.trimIndent()
 }
