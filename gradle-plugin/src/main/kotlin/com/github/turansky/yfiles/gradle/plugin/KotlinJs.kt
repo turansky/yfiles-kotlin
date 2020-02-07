05fd91aed7f62257257c061fd8b89a5f9022288f
Victor Turansky <victor.turansky@gmail.com>
2020-02-07 11:17:15 +0300

    Plugin. Print output file path

---
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/KotlinJs.kt
@@ -0,0 +1,6 @@
+package com.github.turansky.yfiles.gradle.plugin
+
+internal object KotlinJs {
+    val GRADLE_PLUGIN_ID = "org.jetbrains.kotlin.js"
+    val COMPILE_TASK_NAME = "compileKotlinJs"
+}
--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/YFilesGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/YFilesGradlePlugin.kt
@@ -2,9 +2,22 @@ package com.github.turansky.yfiles.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
+import java.io.File
 
 class YFilesGradlePlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        // do nothing
+        target.plugins.withId(KotlinJs.GRADLE_PLUGIN_ID) {
+            target.tasks.asSequence()
+                .filter { it.name == KotlinJs.COMPILE_TASK_NAME }
+                .forEach { it.addJsTransformation() }
+        }
+    }
+}
+
+private fun Task.addJsTransformation() {
+    doLast {
+        val outputFile = property("outputFile") as File
+        println("OUTPUT FILE: $outputFile")
     }
 }
