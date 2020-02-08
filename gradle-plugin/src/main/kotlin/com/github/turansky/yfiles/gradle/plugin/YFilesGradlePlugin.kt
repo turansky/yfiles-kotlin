18490a3ba20e9a54dd78acba1bd2d50a890316f3
Victor Turansky <victor.turansky@gmail.com>
2020-02-08 12:16:02 +0300

    Plugin. Use copy task

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/YFilesGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/YFilesGradlePlugin.kt
@@ -2,41 +2,42 @@ package com.github.turansky.yfiles.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.TaskContainer
+import org.gradle.api.tasks.TaskProvider
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
 import java.io.File
 
 class YFilesGradlePlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.plugins.withId(KotlinJs.GRADLE_PLUGIN_ID) {
-            target.tasks.asSequence()
+            val compileTasks = target.tasks.asSequence()
                 .filter { it.name == KotlinJs.COMPILE_TASK_NAME }
                 .filterIsInstance<KotlinJsCompile>()
-                .forEach { it.addJsTransformation() }
+                .toList()
+
+            target.afterEvaluate {
+                for (compileTask in compileTasks) {
+                    val config = TransformationConfig()
+                    compileTask.addJsTransformation(config)
+                    val copyTask = target.tasks.copyTransformedJs(config)
+                    compileTask.finalizedBy(copyTask)
+                }
+            }
         }
     }
 }
 
-private fun KotlinJsCompile.addJsTransformation() {
-    var configured = false
-    val config = TransformationConfig()
-
-    doFirst {
-        if (!configured) {
-            configured = true
-
-            config.originalOutputFile = property(KotlinJs.OUTPUT_FILE) as File
-            kotlinOptions.outputFile = config.tempOutputFile.absolutePath
-        }
-    }
-
-    doLast {
-        config.apply {
-            copyDirectory(tempOutputDir, originalOutputDir)
-
-            originalOutputFile.writeText("/* GENERATED CONTENT */\n" + tempOutputFile.readText())
-        }
-    }
+private fun KotlinJsCompile.addJsTransformation(config: TransformationConfig) {
+    config.originalOutputFile = property(KotlinJs.OUTPUT_FILE) as File
+    kotlinOptions.outputFile = config.tempOutputFile.absolutePath
 }
+
+private fun TaskContainer.copyTransformedJs(config: TransformationConfig): TaskProvider<*> =
+    register("copyTransformedJs", Copy::class.java) {
+        it.from(config.tempOutputDir)
+        it.into(config.originalOutputDir)
+    }
 
 private class TransformationConfig {
     lateinit var originalOutputFile: File
