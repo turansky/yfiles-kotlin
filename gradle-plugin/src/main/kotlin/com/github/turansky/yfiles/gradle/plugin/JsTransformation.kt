76c80a9738ecf72b7278b21ae69e6b9734c3c148
Victor Turansky <victor.turansky@gmail.com>
2020-02-08 13:53:23 +0300

    Plugin. JS transformation logic in separate file

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/JsTransformation.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/JsTransformation.kt
@@ -1,7 +1,73 @@
+// TODO: Remove after fix https://youtrack.jetbrains.com/issue/KT-34770
 package com.github.turansky.yfiles.gradle.plugin
+
+import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.TaskContainer
+import org.gradle.api.tasks.TaskProvider
+import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
+import java.io.File
 
 private val DESCRIPTOR_REGEX = Regex("(Object\\.defineProperty\\(.+\\.prototype, '[a-zA-Z]+', \\{)")
 
-// TODO: Remove after fix https://youtrack.jetbrains.com/issue/KT-34770
 internal fun String.fixPropertyDeclaration(): String =
     replace(DESCRIPTOR_REGEX, "$1 configurable: true,")
+
+
+internal fun Project.configureJsTransformation() {
+    plugins.withId(KotlinJs.GRADLE_PLUGIN_ID) {
+        val compileTasks = tasks.asSequence()
+            .filter { it.name in KotlinJs.COMPILE_TASK_NAMES }
+            .filterIsInstance<KotlinJsCompile>()
+            .toList()
+
+        afterEvaluate {
+            for (compileTask in compileTasks) {
+                val config = compileTask.addJsTransformation()
+                val copyTask = tasks.copyTransformedJs(compileTask.name, config)
+                compileTask.finalizedBy(copyTask)
+            }
+        }
+    }
+}
+
+private fun KotlinJsCompile.addJsTransformation(): TransformationConfig =
+    TransformationConfig(property(KotlinJs.OUTPUT_FILE) as File).apply {
+        kotlinOptions.outputFile = tempOutputFile.absolutePath
+    }
+
+private fun TaskContainer.copyTransformedJs(
+    compileTaskName: String,
+    config: TransformationConfig
+): TaskProvider<*> =
+    register("copyTransformedJs_$compileTaskName", Copy::class.java) {
+        val outputFileName = config.originalOutputFile.name
+
+        it.from(config.tempOutputDir) {
+            it.include(outputFileName)
+            it.filter { it.fixPropertyDeclaration() }
+        }
+
+        it.from(config.tempOutputDir) {
+            it.exclude(outputFileName)
+        }
+
+        it.into(config.originalOutputDir)
+    }
+
+private class TransformationConfig(val originalOutputFile: File) {
+    val originalOutputDir: File by lazy {
+        originalOutputFile.parentFile
+    }
+
+    val tempOutputDir: File by lazy {
+        originalOutputFile
+            .parentFile
+            .parentFile
+            .resolve("kotlin-temp")
+    }
+
+    val tempOutputFile: File by lazy {
+        tempOutputDir.resolve(originalOutputFile.name)
+    }
+}
--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/YFilesGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/YFilesGradlePlugin.kt
@@ -2,72 +2,9 @@ package com.github.turansky.yfiles.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.Copy
-import org.gradle.api.tasks.TaskContainer
-import org.gradle.api.tasks.TaskProvider
-import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
-import java.io.File
 
 class YFilesGradlePlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.configureJsTransformation()
-    }
-}
-
-private fun Project.configureJsTransformation() {
-    plugins.withId(KotlinJs.GRADLE_PLUGIN_ID) {
-        val compileTasks = tasks.asSequence()
-            .filter { it.name in KotlinJs.COMPILE_TASK_NAMES }
-            .filterIsInstance<KotlinJsCompile>()
-            .toList()
-
-        afterEvaluate {
-            for (compileTask in compileTasks) {
-                val config = compileTask.addJsTransformation()
-                val copyTask = tasks.copyTransformedJs(compileTask.name, config)
-                compileTask.finalizedBy(copyTask)
-            }
-        }
-    }
-}
-
-private fun KotlinJsCompile.addJsTransformation(): TransformationConfig =
-    TransformationConfig(property(KotlinJs.OUTPUT_FILE) as File).apply {
-        kotlinOptions.outputFile = tempOutputFile.absolutePath
-    }
-
-private fun TaskContainer.copyTransformedJs(
-    compileTaskName: String,
-    config: TransformationConfig
-): TaskProvider<*> =
-    register("copyTransformedJs_$compileTaskName", Copy::class.java) {
-        val outputFileName = config.originalOutputFile.name
-
-        it.from(config.tempOutputDir) {
-            it.include(outputFileName)
-            it.filter { it.fixPropertyDeclaration() }
-        }
-
-        it.from(config.tempOutputDir) {
-            it.exclude(outputFileName)
-        }
-
-        it.into(config.originalOutputDir)
-    }
-
-private class TransformationConfig(val originalOutputFile: File) {
-    val originalOutputDir: File by lazy {
-        originalOutputFile.parentFile
-    }
-
-    val tempOutputDir: File by lazy {
-        originalOutputFile
-            .parentFile
-            .parentFile
-            .resolve("kotlin-temp")
-    }
-
-    val tempOutputFile: File by lazy {
-        tempOutputDir.resolve(originalOutputFile.name)
     }
 }
