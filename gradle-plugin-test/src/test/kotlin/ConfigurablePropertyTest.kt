ac2a9e61ed6bf6683372dd921a4c711a2280d6cf
Victor Turansky <victor.turansky@gmail.com>
2020-02-09 01:07:49 +0300

    Plugin. Test for configurable properties

---
+++ gradle-plugin-test/src/test/kotlin/ConfigurablePropertyTest.kt
@@ -0,0 +1,18 @@
+import Object.Companion.getOwnPropertyDescriptor
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class ConfigurablePropertyTest {
+    @Test
+    fun arrowProperties() {
+        val jsClassPrototype: Any = CustomArrow::class.js.asDynamic().prototype
+
+        assertTrue {
+            getOwnPropertyDescriptor(jsClassPrototype, "cropLength").configurable
+        }
+
+        assertTrue {
+            getOwnPropertyDescriptor(jsClassPrototype, "length").configurable
+        }
+    }
+}
