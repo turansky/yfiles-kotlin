817fdb700a5ca504f3f6de815adebb4e13882f6d
Victor Turansky <victor.turansky@gmail.com>
2020-03-03 22:17:11 +0300

    Revert "Remove IR extension"
    This reverts commit 44ff0430

---
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/extensions/YRegistrar.kt
@@ -0,0 +1,16 @@
+package com.github.turansky.yfiles.compiler.extensions
+
+import com.github.turansky.yfiles.compiler.backend.js.JsExtension
+import org.jetbrains.kotlin.com.intellij.mock.MockProject
+import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.js.translate.extensions.JsSyntheticTranslateExtension
+
+class YRegistrar : ComponentRegistrar {
+    override fun registerProjectComponents(
+        project: MockProject,
+        configuration: CompilerConfiguration
+    ) {
+        JsSyntheticTranslateExtension.registerExtension(project, JsExtension())
+    }
+}
