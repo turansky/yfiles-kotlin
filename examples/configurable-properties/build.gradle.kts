4b3a4dfd6622cc0e6020ed6fdd0caf72a5c292d9
Victor Turansky <victor.turansky@gmail.com>
2020-04-16 14:05:20 +0300

    IR test classes

--- examples/configurable-properties/build.gradle.kts
+++ examples/configurable-properties/build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     kotlin("js")
     id("com.github.turansky.yfiles")
+    id("com.github.turansky.kfc.webpack")
 }
 
 kotlin.js {
     browser()
+
+    binaries.executable()
 }
 
 dependencies {
--- examples/configurable-properties/src/main/kotlin/AbstractArrow.kt
+++ examples/configurable-properties/src/main/kotlin/AbstractArrow.kt
@@ -1,8 +1,17 @@
 import yfiles.styles.IArrow
 
+@JsExport
+@ExperimentalJsExport
 abstract class AbstractArrow : IArrow {
     override val cropLength = 13.0
     override val length = 42.0
+}
+
+@JsExport
+@ExperimentalJsExport
+abstract class AbstractArrow2 : AbstractArrow() {
+    override val cropLength = 14.0
+    override val length = 43.0
 }
 
 abstract class ZArrow {
