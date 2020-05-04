3b9811b8b9fa877320ab8679efecdda3db345ca4
Victor Turansky <victor.turansky@gmail.com>
2020-05-04 04:23:36 +0300

    Update collection util

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/Collections.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/Collections.kt
@@ -1,13 +1,10 @@
 package com.github.turansky.yfiles
 
-internal fun <T> Iterable<T>.byComma(transform: ((T) -> CharSequence)? = null): String {
-    return joinToString(separator = ", ", transform = transform)
-}
+internal fun <T> Iterable<T>.byComma(transform: ((T) -> CharSequence)? = null): String =
+    joinToString(separator = ", ", transform = transform)
 
-internal fun <T> Iterable<T>.byCommaLine(transform: ((T) -> CharSequence)? = null): String {
-    return joinToString(separator = ",\n", transform = transform)
-}
+internal fun <T> Iterable<T>.byCommaLine(transform: ((T) -> CharSequence)? = null): String =
+    joinToString(separator = ",\n", transform = transform)
 
-internal fun <T> Iterable<T>.lines(transform: ((T) -> CharSequence)? = null): String {
-    return joinToString(separator = "\n", postfix = "\n", transform = transform)
-}
+internal fun <T> Iterable<T>.lines(transform: ((T) -> CharSequence)? = null): String =
+    joinToString(separator = "\n", postfix = "\n", transform = transform)
