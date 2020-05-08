10f5ee2d8d21a43794e33e0ffd41acd96d6790b0
Victor Turansky <victor.turansky@gmail.com>
2020-05-08 06:12:58 +0300

    Describe TimeSpan extensions

---
+++ examples/simple-app/src/main/kotlin/TimeSpan.kt
@@ -0,0 +1,10 @@
+@file:Suppress("unused")
+
+import yfiles.lang.*
+
+fun timeSpan() {
+    val c: TimeSpan = 2.hours
+    val o: TimeSpan = 0.minutes
+    val d: TimeSpan = 1.seconds
+    val e: TimeSpan = 3.milliseconds
+}
