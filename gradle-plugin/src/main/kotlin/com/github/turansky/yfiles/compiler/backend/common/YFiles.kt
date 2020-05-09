0c31b72b26020a182358192ddd0502edb9c41666
Victor Turansky <victor.turansky@gmail.com>
2020-05-09 04:29:19 +0300

    JsExtension specific in JS package

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/backend/common/YFiles.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/backend/common/YFiles.kt
@@ -1,10 +1,7 @@
 package com.github.turansky.yfiles.compiler.backend.common
 
-import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name.identifier
-import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
-import org.jetbrains.kotlin.resolve.descriptorUtil.getSuperInterfaces
 
 internal val YFILES_PACKAGE = FqName("yfiles")
 internal val LANG_PACKAGE = YFILES_PACKAGE.child(identifier("lang"))
@@ -13,31 +10,4 @@ internal val YOBJECT = LANG_PACKAGE.child(identifier("YObject"))
 internal val YCLASS_NAME = identifier("YClass")
 internal val BASE_CLASS_NAME = identifier("BaseClass")
 
-private val CLASS_METADATA = LANG_PACKAGE.child(identifier("ClassMetadata"))
-
-private val ClassDescriptor.isYObject: Boolean
-    get() = isExternal && fqNameSafe == YOBJECT
-
-internal fun ClassDescriptor.isYFilesInterface(): Boolean =
-    isExternal and (isYObject or implementsYObject)
-
-internal val ClassDescriptor.implementsYObjectDirectly: Boolean
-    get() = getSuperInterfaces()
-        .any { it.isYObject }
-
-private val ClassDescriptor.implementsYObject: Boolean
-    get() {
-        if (implementsYObjectDirectly) {
-            return true
-        }
-
-        return getSuperInterfaces()
-            .any { it.implementsYObject }
-    }
-
-internal val ClassDescriptor.implementsYFilesInterface: Boolean
-    get() = getSuperInterfaces()
-        .any { it.isYFilesInterface() }
-
-internal fun ClassDescriptor.asClassMetadata(): ClassDescriptor? =
-    getSuperInterfaces().firstOrNull { it.fqNameSafe == CLASS_METADATA }
+internal val CLASS_METADATA = LANG_PACKAGE.child(identifier("ClassMetadata"))
