e55ea918d639423833a6e360f837090e408754c7
Victor Turansky <victor.turansky@gmail.com>
2020-05-09 22:32:42 +0300

    YDeclarationChecker. Add interface check

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/backend/common/YDeclarationChecker.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/backend/common/YDeclarationChecker.kt
@@ -1,5 +1,8 @@
 package com.github.turansky.yfiles.compiler.backend.common
 
+import com.github.turansky.yfiles.compiler.diagnostic.BaseClassErrors
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.diagnostics.DiagnosticFactory0
 import org.jetbrains.kotlin.psi.KtClassOrObject
@@ -13,13 +16,45 @@ object YDeclarationChecker : DeclarationChecker {
         descriptor: DeclarationDescriptor,
         context: DeclarationCheckerContext
     ) {
+        if (declaration !is KtClassOrObject) return
+        if (descriptor !is ClassDescriptor) return
+        if (descriptor.isExternal) return
+
+        when (descriptor.kind) {
+            ClassKind.CLASS
+            -> context.checkClass(declaration, descriptor)
+
+            ClassKind.OBJECT,
+            ClassKind.INTERFACE,
+            ClassKind.ENUM_CLASS
+            -> context.checkInterfaces(declaration, descriptor)
+
+            else -> {
+                // do nothing
+            }
+        }
+    }
+
+    private fun DeclarationCheckerContext.checkClass(
+        declaration: KtClassOrObject,
+        descriptor: ClassDescriptor
+    ) {
         // implement
     }
 
+    private fun DeclarationCheckerContext.checkInterfaces(
+        declaration: KtClassOrObject,
+        descriptor: ClassDescriptor
+    ) {
+        if (descriptor.implementsYFilesInterface) {
+            reportError(declaration, BaseClassErrors.INTERFACE_IMPLEMENTING_NOT_SUPPORTED)
+        }
+    }
+
     private fun DeclarationCheckerContext.reportError(
-        classOrObject: KtClassOrObject,
+        declaration: KtClassOrObject,
         diagnosticFactory: DiagnosticFactory0<KtClassOrObject>
     ) {
-        trace.report(diagnosticFactory.on(classOrObject))
+        trace.report(diagnosticFactory.on(declaration))
     }
 }
