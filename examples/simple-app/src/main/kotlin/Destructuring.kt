22a9087c1c9cf7313cf449433922c400207f1da1
Victor Turansky <victor.turansky@gmail.com>
2020-05-18 02:48:27 +0300

    'destructing' -> 'destructuring'

--- examples/simple-app/src/main/kotlin/Destructuring.kt
+++ examples/simple-app/src/main/kotlin/Destructuring.kt
@@ -5,7 +5,7 @@ import yfiles.algorithms.component1
 import yfiles.algorithms.component2
 import yfiles.algorithms.plus
 
-fun destructing() {
+fun destructuring() {
     val p1 = YPoint(4.0, 8.0)
     val p2 = YPoint(15.0, 16.0)
     val p3 = YPoint(23.0, 42.0)
