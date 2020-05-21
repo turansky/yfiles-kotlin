c18320e52007aab72c18dc94e7d05cb2ac01f973
Victor Turansky <victor.turansky@gmail.com>
2020-05-21 03:58:26 +0300

    Summary cleanup 'Number' -> 'Double'

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/Summary.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/Summary.kt
@@ -32,6 +32,7 @@ private fun String.fixApiLinks(): String {
         .replace("[$JS_STRING]", "[$STRING]")
         .replace("[$JS_NUMBER]", "[Number]")
         .replace("[$JS_NUMBER.", "[$DOUBLE.")
+        .replace("[Number.", "[$DOUBLE.")
         .replace("[$JS_BOOLEAN]", "[$BOOLEAN]")
         .replace("more NaN values", "more `NaN` values")
         .replace(">evt<", ">event<")
