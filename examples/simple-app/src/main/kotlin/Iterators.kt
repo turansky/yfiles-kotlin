16207fbaa7a0f349b25d077fc4db6e1a7bbecac2
Victor Turansky <victor.turansky@gmail.com>
2020-05-24 16:32:47 +0300

    Add Iterator usage examples

---
+++ examples/simple-app/src/main/kotlin/Iterators.kt
@@ -0,0 +1,38 @@
+@file:Suppress("unused")
+
+import yfiles.collections.iterator
+import yfiles.graph.DefaultGraph
+import yfiles.graph.IGraph
+import yfiles.graph.applyLayout
+import yfiles.hierarchic.HierarchicLayout
+import yfiles.layout.DefaultLayoutGraph
+import yfiles.layout.LayoutGraph
+
+fun enumerableIterator() {
+    val graph: IGraph = DefaultGraph {
+        createNode()
+        createNode()
+        createNode()
+
+        applyLayout(HierarchicLayout())
+    }
+
+    for (node in graph.nodes) {
+        println("Node layout: ${node.layout}")
+    }
+}
+
+fun cursorIterator() {
+    val graph: LayoutGraph = DefaultLayoutGraph().apply {
+        createNode()
+        createNode()
+        createNode()
+    }
+
+    HierarchicLayout()
+        .applyLayout(graph)
+
+    for (node in graph.nodes) {
+        println("Node index: ${node.index}")
+    }
+}
