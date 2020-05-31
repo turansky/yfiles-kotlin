4e2bae310d759cf0b2602a672e1efde2108a158a
Victor Turansky <victor.turansky@gmail.com>
2020-05-31 12:58:04 +0300

    LayoutGraphAdapter. Strict types

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/LayoutStrictTypes.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/LayoutStrictTypes.kt
@@ -1,12 +1,20 @@
 package com.github.turansky.yfiles.correction
 
+import com.github.turansky.yfiles.IENUMERABLE
+import com.github.turansky.yfiles.JS_ANY
 import com.github.turansky.yfiles.JS_OBJECT
 import com.github.turansky.yfiles.YOBJECT
 
 internal fun applyLayoutStrictTypes(source: Source) {
-    source.type("CopiedLayoutGraph")
+    source.types("CopiedLayoutGraph", "LayoutGraphAdapter")
         .flatMap(METHODS)
         .flatMap { it.optFlatMap(PARAMETERS) + it.returnsSequence() }
         .filter { it[TYPE] == JS_OBJECT }
         .forEach { it[TYPE] = YOBJECT }
+
+    source.type("LayoutGraphAdapter")
+        .let { type -> sequenceOf("nodeObjects", "edgeObjects").map { type.method(it) } }
+        .map { it[RETURNS] }
+        .onEach { check(it[TYPE] == "$IENUMERABLE<$JS_ANY>") }
+        .forEach { it[TYPE] = "$IENUMERABLE<$YOBJECT>" }
 }
