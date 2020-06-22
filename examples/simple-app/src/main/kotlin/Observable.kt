f245593de4f976a3ad377762cd86ae1f479fc3fd
Victor Turansky <victor.turansky@gmail.com>
2020-06-22 22:19:59 +0300

    'observable' example

---
+++ examples/simple-app/src/main/kotlin/Observable.kt
@@ -0,0 +1,15 @@
+@file:Suppress("unused", "UNUSED_VARIABLE")
+
+import yfiles.lang.observable
+
+class Point3D {
+    var x: Double by observable(0.0)
+    var y: Double by observable(0.0)
+    var z: Double by observable(0.0)
+}
+
+class User {
+    var firstName: String by observable("Frodo")
+    var lastName: String by observable("Baggins")
+    var age: Int by observable(42)
+}
