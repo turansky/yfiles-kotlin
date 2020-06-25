78f24b9eecd280eba564f9a3dd368a4f89b46cde
Victor Turansky <victor.turansky@gmail.com>
2020-06-25 19:15:59 +0300

    Update DiagnosticSuppressor

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/diagnostic/YDiagnosticSuppressor.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/diagnostic/YDiagnosticSuppressor.kt
@@ -1,10 +1,20 @@
 package com.github.turansky.yfiles.compiler.diagnostic
 
 import org.jetbrains.kotlin.diagnostics.Diagnostic
+import org.jetbrains.kotlin.diagnostics.DiagnosticFactory
+import org.jetbrains.kotlin.diagnostics.Errors.USELESS_IS_CHECK
+import org.jetbrains.kotlin.js.resolve.diagnostics.ErrorsJs.CANNOT_CHECK_FOR_EXTERNAL_INTERFACE
+import org.jetbrains.kotlin.js.resolve.diagnostics.ErrorsJs.UNCHECKED_CAST_TO_EXTERNAL_INTERFACE
 import org.jetbrains.kotlin.resolve.diagnostics.DiagnosticSuppressor
 
 class YDiagnosticSuppressor : DiagnosticSuppressor {
+    private val suppresssed: Set<DiagnosticFactory<*>> = setOf(
+        CANNOT_CHECK_FOR_EXTERNAL_INTERFACE,
+        UNCHECKED_CAST_TO_EXTERNAL_INTERFACE,
+        USELESS_IS_CHECK
+    )
+
     override fun isSuppressed(diagnostic: Diagnostic): Boolean {
-        return false
+        return diagnostic.factory in suppresssed
     }
 }
