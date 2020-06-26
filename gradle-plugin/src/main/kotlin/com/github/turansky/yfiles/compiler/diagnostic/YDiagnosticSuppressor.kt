448f6cf668e5b6f1c5ee28e3b3098a904d11344c
Victor Turansky <victor.turansky@gmail.com>
2020-06-26 05:24:18 +0300

    Separate factory checks

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/diagnostic/YDiagnosticSuppressor.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/diagnostic/YDiagnosticSuppressor.kt
@@ -5,16 +5,32 @@ import org.jetbrains.kotlin.diagnostics.DiagnosticFactory
 import org.jetbrains.kotlin.diagnostics.Errors.USELESS_IS_CHECK
 import org.jetbrains.kotlin.js.resolve.diagnostics.ErrorsJs.CANNOT_CHECK_FOR_EXTERNAL_INTERFACE
 import org.jetbrains.kotlin.js.resolve.diagnostics.ErrorsJs.UNCHECKED_CAST_TO_EXTERNAL_INTERFACE
+import org.jetbrains.kotlin.psi.KtBinaryExpressionWithTypeRHS
+import org.jetbrains.kotlin.psi.KtIsExpression
 import org.jetbrains.kotlin.resolve.diagnostics.DiagnosticSuppressor
 
-class YDiagnosticSuppressor : DiagnosticSuppressor {
-    private val suppressedFactories: Set<DiagnosticFactory<*>> = setOf(
-        CANNOT_CHECK_FOR_EXTERNAL_INTERFACE,
-        UNCHECKED_CAST_TO_EXTERNAL_INTERFACE,
-        USELESS_IS_CHECK
-    )
+private val IS_FACTORIES: Set<DiagnosticFactory<*>> = setOf(
+    CANNOT_CHECK_FOR_EXTERNAL_INTERFACE,
+    USELESS_IS_CHECK
+)
 
+private val AS_FACTORIES: Set<DiagnosticFactory<*>> = setOf(
+    UNCHECKED_CAST_TO_EXTERNAL_INTERFACE
+)
+
+class YDiagnosticSuppressor : DiagnosticSuppressor {
     override fun isSuppressed(diagnostic: Diagnostic): Boolean {
-        return diagnostic.factory in suppressedFactories
+        val factory = diagnostic.factory
+        val psiElement = diagnostic.psiElement
+
+        return when (psiElement) {
+            is KtIsExpression
+            -> factory in IS_FACTORIES
+
+            is KtBinaryExpressionWithTypeRHS
+            -> factory in AS_FACTORIES
+
+            else -> false
+        }
     }
 }
