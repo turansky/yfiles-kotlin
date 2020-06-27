f9c7fd1f3bc212478bf6decc7671f2ca512bbc96
Victor Turansky <victor.turansky@gmail.com>
2020-06-27 03:59:26 +0300

    Add AbstractArrow2

--- examples/configurable-properties/src/main/kotlin/AbstractArrow.kt
+++ examples/configurable-properties/src/main/kotlin/AbstractArrow.kt
@@ -1,10 +1,35 @@
+import yfiles.graph.INode
+import yfiles.input.IHitTestable
 import yfiles.styles.IArrow
+import yfiles.view.IVisualCreator
 
 @JsExport
 @ExperimentalJsExport
 abstract class AbstractArrow : IArrow {
     override val cropLength = 13.0
     override val length = 42.0
+}
+
+@JsExport
+@ExperimentalJsExport
+abstract class AbstractArrow2 : AbstractArrow() {
+    override val cropLength = 14.0
+    override val length = 43.0
+
+    fun castTest() {
+        val i = js("({})")
+
+        val t1 = i as IHitTestable
+        val t2 = i as IVisualCreator
+        val t3 = i as INode
+
+        val is1 = i is IHitTestable
+        val is2 = i is IVisualCreator
+        val is3 = i is INode
+
+        println(t1)
+        println(t2)
+    }
 }
 
 abstract class ZArrow {
