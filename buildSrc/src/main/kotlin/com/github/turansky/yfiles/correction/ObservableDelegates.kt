d7296f31b4077e68fdf629a0203dd60fc3aaa33e
Victor Turansky <victor.turansky@gmail.com>
2020-06-28 19:23:20 +0300

    Observable tag properties

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/ObservableDelegates.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/ObservableDelegates.kt
@@ -2,20 +2,58 @@ package com.github.turansky.yfiles.correction
 
 import com.github.turansky.yfiles.GeneratorContext
 import com.github.turansky.yfiles.KPROPERTY
+import com.github.turansky.yfiles.READ_ONLY_PROPERTY
 import com.github.turansky.yfiles.READ_WRITE_PROPERTY
 
-internal val OBSERVABLE = "yfiles.lang.Observable"
+internal val OBSERVABLE = "yfiles.graph.Observable"
+internal val MAKE_OBSERVABLE = "yfiles.styles.Templates.makeObservable"
 
 internal fun generateObservableDelegates(context: GeneratorContext) {
     // language=kotlin
     context[OBSERVABLE] =
         """
+        fun <T: Any> $TAG.observable(o: T): IPropertyProvider<$TAG, T> {
+            if (firePropertyChangedDeclared) {
+                $MAKE_OBSERVABLE(this)
+            }
+            
+            return TagPropertyProvider(o)
+        }    
+            
         fun <T> Any.observable(initialValue: T): $READ_WRITE_PROPERTY<Any, T> {
             if (firePropertyChangedDeclared) {
-                yfiles.styles.Templates.makeObservable(this)
+                $MAKE_OBSERVABLE(this)
             }
         
             return Observable(initialValue)
+        }
+        
+        interface IPropertyProvider<P:Any, T:Any> {
+            operator fun provideDelegate(
+                thisRef: P,
+                property: $KPROPERTY<*>
+            ): $READ_ONLY_PROPERTY<P, T>
+        }
+        
+        private class TagPropertyProvider<T:Any>(
+            private val value: T
+        ): IPropertyProvider<$TAG, T> {
+            override operator fun provideDelegate(
+                thisRef: $TAG,
+                property: $KPROPERTY<*>
+            ): $READ_ONLY_PROPERTY<$TAG, T> {
+                return Constant(value)
+            }
+        }
+        
+        private class Constant<P:Any, T:Any>(
+            private val value: T
+        ) : $READ_ONLY_PROPERTY<P, T> {
+            override fun getValue(
+                thisRef: P,
+                property: $KPROPERTY<*>
+            ): T =
+                value
         }
         
         private class Observable<T>(
