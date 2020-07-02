356b536b7a5f46823c7b397f36f12de4c01ce626
Victor Turansky <victor.turansky@gmail.com>
2020-07-02 20:26:36 +0300

    Add custom diagnostic suppressor

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/extensions/YRegistrar.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/extensions/YRegistrar.kt
@@ -1,11 +1,14 @@
 package com.github.turansky.yfiles.compiler.extensions
 
 import com.github.turansky.yfiles.compiler.backend.js.JsExtension
+import com.github.turansky.yfiles.compiler.diagnostic.YDiagnosticSuppressor
 import org.jetbrains.kotlin.com.intellij.mock.MockProject
+import org.jetbrains.kotlin.com.intellij.openapi.extensions.Extensions
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.extensions.StorageComponentContainerContributor
 import org.jetbrains.kotlin.js.translate.extensions.JsSyntheticTranslateExtension
+import org.jetbrains.kotlin.resolve.diagnostics.DiagnosticSuppressor
 
 class YRegistrar : ComponentRegistrar {
     override fun registerProjectComponents(
@@ -15,5 +18,9 @@ class YRegistrar : ComponentRegistrar {
         JsSyntheticTranslateExtension.registerExtension(project, JsExtension())
 
         StorageComponentContainerContributor.registerExtension(project, YStorageComponentContainerContributor())
+
+        Extensions.getRootArea()
+            .getExtensionPoint(DiagnosticSuppressor.EP_NAME)
+            .registerExtension(YDiagnosticSuppressor()) { /* do nothing */ }
     }
 }
