a2c5e62e07f26a542f2ed385b23d88bbe40165ad
Victor Turansky <victor.turansky@gmail.com>
2020-07-02 22:14:25 +0300

    Update "lookup" example

--- examples/cast/src/main/kotlin/Lookups.kt
+++ examples/cast/src/main/kotlin/Lookups.kt
@@ -14,25 +14,16 @@ fun lookups() {
     val node = graph.createNode()
 
     // for classes
-    val t11 = node lookup TimeSpan.yclass // 'TimeSpan?'
-    val t12 = node lookup TimeSpan        // 'TimeSpan?'
+    val t13: TimeSpan? = node.lookup()      // reified lookup type
+    val t14 = node.lookup<TimeSpan>()       // 'TimeSpan?'
 
-    val t13: TimeSpan? = node.lookup()    // reified lookup type
-    val t14 = node.lookup<TimeSpan>()     // 'TimeSpan?'
-
-    val t21 = node lookupValue TimeSpan.yclass // 'TimeSpan'
-    val t22 = node lookupValue TimeSpan        // 'TimeSpan'
-
-    val t23: TimeSpan = node.lookupValue()    // reified lookup type
-    val t24 = node.lookupValue<TimeSpan>()    // 'TimeSpan'
+    val t23: TimeSpan = node.lookupValue()  // reified lookup type
+    val t24 = node.lookupValue<TimeSpan>()  // 'TimeSpan'
 
     // for interfaces
-    val h11 = node lookup IHitTestable.yclass      // 'IHitTestable?'
-    val h12 = node lookup IHitTestable             // 'IHitTestable?'
+    val h13: IHitTestable? = node.lookup()      // reified lookup type
+    val h14 = node.lookup<IHitTestable>()       // 'IHitTestable?'
 
-    val h21 = node lookupValue IHitTestable.yclass // 'IHitTestable'
-    val h22 = node lookupValue IHitTestable        // 'IHitTestable'
-
-    val h23: IHitTestable = node.lookupValue()    // reified lookup type
-    val h24 = node.lookupValue<IHitTestable>()    // 'TimeSpan'
+    val h23: IHitTestable = node.lookupValue()  // reified lookup type
+    val h24 = node.lookupValue<IHitTestable>()  // 'IHitTestable'
 }
