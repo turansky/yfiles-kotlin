11ccf0906d8e5aeead371024bb66f72bfb4eb90d
Victor Turansky <victor.turansky@gmail.com>
2020-07-03 04:13:38 +0300

    Merge fix

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/diagnostic/YDiagnosticSuppressor.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/diagnostic/YDiagnosticSuppressor.kt
@@ -6,30 +6,20 @@ import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.diagnostics.DiagnosticFactory
 import org.jetbrains.kotlin.diagnostics.DiagnosticWithParameters1
-import org.jetbrains.kotlin.js.resolve.diagnostics.ErrorsJs.EXTERNAL_INTERFACE_AS_REIFIED_TYPE_ARGUMENT
-import org.jetbrains.kotlin.js.resolve.diagnostics.ErrorsJs.NESTED_CLASS_IN_EXTERNAL_INTERFACE
+import org.jetbrains.kotlin.diagnostics.Errors.USELESS_IS_CHECK
+import org.jetbrains.kotlin.js.resolve.diagnostics.ErrorsJs.*
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.diagnostics.DiagnosticSuppressor
 import org.jetbrains.kotlin.types.KotlinType
 
 private val IS_FACTORIES: Set<DiagnosticFactory<*>> = setOf(
-    // TODO: use in IR
-    /*
     CANNOT_CHECK_FOR_EXTERNAL_INTERFACE,
     USELESS_IS_CHECK
-    */
 )
 
 private val AS_FACTORIES: Set<DiagnosticFactory<*>> = setOf(
-    // TODO: use in IR
-    /*
     UNCHECKED_CAST_TO_EXTERNAL_INTERFACE
-    */
-)
-
-private val REIFIED_TYPE_FACTORIES: Set<DiagnosticFactory<*>> = setOf(
-    EXTERNAL_INTERFACE_AS_REIFIED_TYPE_ARGUMENT
 )
 
 class YDiagnosticSuppressor : DiagnosticSuppressor {
