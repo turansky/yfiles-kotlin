1fce0ca99cc2071f4023f48447bc6da2cd4a0373
Victor Turansky <victor.turansky@gmail.com>
2020-07-05 00:55:31 +0300

    Enable 'external fun interface'

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/diagnostic/YDiagnosticSuppressor.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/diagnostic/YDiagnosticSuppressor.kt
@@ -6,8 +6,7 @@ import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.diagnostics.DiagnosticFactory
 import org.jetbrains.kotlin.diagnostics.DiagnosticWithParameters1
-import org.jetbrains.kotlin.js.resolve.diagnostics.ErrorsJs.EXTERNAL_INTERFACE_AS_REIFIED_TYPE_ARGUMENT
-import org.jetbrains.kotlin.js.resolve.diagnostics.ErrorsJs.NESTED_CLASS_IN_EXTERNAL_INTERFACE
+import org.jetbrains.kotlin.js.resolve.diagnostics.ErrorsJs.*
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.diagnostics.DiagnosticSuppressor
@@ -61,6 +60,11 @@ class YDiagnosticSuppressor : DiagnosticSuppressor {
             -> factory === NESTED_CLASS_IN_EXTERNAL_INTERFACE
                     && psiElement.isYFilesInterfaceCompanion(bindingContext)
 
+            is KtClass
+            -> factory === WRONG_EXTERNAL_DECLARATION
+                    && diagnostic.funInterfaceCheck
+                    && psiElement.isYFilesInterface(bindingContext)
+
             else -> false
         }
     }
@@ -72,11 +76,22 @@ private val Diagnostic.reifiedType: KotlinType?
         else -> null
     }
 
+private val Diagnostic.funInterfaceCheck: Boolean
+    get() = this is DiagnosticWithParameters1<*, *>
+            && a == "fun interface"
+
 private fun KtTypeReference?.isYFilesInterface(
     context: BindingContext
 ): Boolean =
     context[BindingContext.TYPE, this]
         .isYFilesInterface()
+
+private fun KtClass.isYFilesInterface(
+    context: BindingContext
+): Boolean {
+    val descriptor = context[BindingContext.CLASS, this] ?: return false
+    return descriptor.isYFilesInterface()
+}
 
 private fun KotlinType?.isYFilesInterface(): Boolean {
     this ?: return false
