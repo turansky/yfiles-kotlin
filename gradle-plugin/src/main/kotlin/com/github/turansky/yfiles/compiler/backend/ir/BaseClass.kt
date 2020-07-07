2e090a5add92f2ba07fbacfc2df08e5a96e91b6a
Victor Turansky <victor.turansky@gmail.com>
2020-07-07 18:44:39 +0300

    Use 'createParameterDeclarations'

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/backend/ir/BaseClass.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/backend/ir/BaseClass.kt
@@ -1,16 +1,16 @@
 package com.github.turansky.yfiles.compiler.backend.ir
 
-import org.jetbrains.kotlin.backend.common.ir.createImplicitParameterDeclarationWithWrappedDescriptor
+import org.jetbrains.kotlin.backend.common.ir.createParameterDeclarations
 import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.descriptors.Visibilities
-import org.jetbrains.kotlin.ir.backend.js.ir.JsIrBuilder
 import org.jetbrains.kotlin.ir.builders.declarations.buildClass
 import org.jetbrains.kotlin.ir.declarations.IrClass
+import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 import org.jetbrains.kotlin.name.Name
 
 internal fun baseClass(): IrClass = buildClass {
-    origin = JsIrBuilder.SYNTHESIZED_DECLARATION
+    origin = IrDeclarationOrigin.FILE_CLASS
 
     name = Name.identifier("BaseClass_YYYY")
     kind = ClassKind.CLASS
@@ -18,5 +18,5 @@ internal fun baseClass(): IrClass = buildClass {
     modality = Modality.ABSTRACT
     isExternal = true
 }.apply {
-    createImplicitParameterDeclarationWithWrappedDescriptor()
+    createParameterDeclarations()
 }
