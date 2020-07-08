86003237ec022fcbce4210d99b4349dc4907d071
Victor Turansky <victor.turansky@gmail.com>
2020-07-08 22:18:49 +0300

    Declare supported type operators

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/backend/ir/YCastTransformer.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/backend/ir/YCastTransformer.kt
@@ -2,6 +2,8 @@ package com.github.turansky.yfiles.compiler.backend.ir
 
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.expressions.IrExpression
+import org.jetbrains.kotlin.ir.expressions.IrTypeOperator
+import org.jetbrains.kotlin.ir.expressions.IrTypeOperator.*
 import org.jetbrains.kotlin.ir.expressions.IrTypeOperatorCall
 import org.jetbrains.kotlin.ir.types.IrType
 import org.jetbrains.kotlin.ir.types.getClass
@@ -10,11 +12,22 @@ import org.jetbrains.kotlin.ir.util.defaultType
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
 
+private val SUPPORTED_OPERATORS: Set<IrTypeOperator> = setOf(
+    INSTANCEOF,
+    NOT_INSTANCEOF,
+
+    CAST,
+    SAFE_CAST
+)
+
 internal class YCastTransformer : IrElementTransformerVoid() {
     override fun visitTypeOperator(
         expression: IrTypeOperatorCall
     ): IrExpression {
         expression.transformChildrenVoid(this)
+
+        if (expression.operator !in SUPPORTED_OPERATORS)
+            return expression
 
         val type = expression.typeOperand.getClass()
             ?.takeIf { it.isYFilesInterface() }
