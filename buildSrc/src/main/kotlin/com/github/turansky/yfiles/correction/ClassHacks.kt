3cb0a5c8e40fbf0c2def9856f2406f4772a9256e
Victor Turansky <victor.turansky@gmail.com>
2020-07-09 16:19:33 +0300

    Add WA for 'yclass' property

--- buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/ClassHacks.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/yfiles/correction/ClassHacks.kt
@@ -72,7 +72,11 @@ internal fun generateClassUtils(context: GeneratorContext) {
                 
             private class SimpleClassMetadata<T: $YOBJECT>(
                 override val yclass: $YCLASS<T>
-            ): $ICLASS_METADATA<T>
+            ): $ICLASS_METADATA<T> {
+                // WA: https://youtrack.jetbrains.com/issue/KT-40155
+                @JsName("yclass")
+                private val yclassDelegate: $YCLASS<T> = yclass
+            }
         """.trimIndent()
 
     // language=kotlin
