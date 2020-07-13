80a8fd64453df7ae9d2daca992c59ae496fd3af4
Victor Turansky <victor.turansky@gmail.com>
2020-07-14 00:41:06 +0300

    Support 'YEnum.valueOf' method

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/backend/ir/EnumTransformer.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/compiler/backend/ir/EnumTransformer.kt
@@ -7,7 +7,6 @@ import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.impl.IrCallImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrGetObjectValueImpl
-import org.jetbrains.kotlin.ir.types.defaultType
 import org.jetbrains.kotlin.ir.types.getClass
 import org.jetbrains.kotlin.ir.util.companionObject
 import org.jetbrains.kotlin.ir.util.defaultType
@@ -18,13 +17,16 @@ import org.jetbrains.kotlin.name.Name
 
 private val GET_ENUM_NAME = FqName("yfiles.lang.getEnumName")
 private val GET_ENUM_ORDINAL = FqName("yfiles.lang.getEnumOrdinal")
+
 private val GET_ENUM_VALUES = FqName("yfiles.lang.getEnumValues")
+private val GET_ENUM_VALUE_OF = FqName("yfiles.lang.getEnumValueOf")
 
 private val GET_NAME = Name.special("<get-name>")
 private val GET_ORDINAL = Name.special("<get-ordinal>")
 private val TO_STRING = Name.identifier("toString")
 
 private val VALUES = Name.identifier("values")
+private val VALUE_OF = Name.identifier("valueOf")
 
 private val IrClass.isYFilesEnum
     get() = isExternal && isEnumClass
@@ -44,7 +46,8 @@ internal class EnumTransformer(
                 TO_STRING
                 -> parent.isYFilesEnum
 
-                VALUES
+                VALUES,
+                VALUE_OF
                 -> parent.isYFilesEnum
 
                 else -> false
@@ -62,6 +65,7 @@ internal class EnumTransformer(
             TO_STRING -> createCall(expression, GET_ENUM_NAME)
 
             VALUES -> createStaticCall(expression, GET_ENUM_VALUES)
+            VALUE_OF -> createStaticCall(expression, GET_ENUM_VALUE_OF)
 
             else -> null
         }
@@ -113,7 +117,7 @@ internal class EnumTransformer(
         val call = IrCallImpl(
             startOffset = sourceCall.startOffset,
             endOffset = sourceCall.endOffset,
-            type = context.symbols.array.defaultType,
+            type = enumClass.defaultType,
             symbol = function
         )
 
@@ -126,6 +130,10 @@ internal class EnumTransformer(
 
         call.putTypeArgument(0, enumClass.defaultType)
         call.putValueArgument(0, typeParameter)
+
+        if (sourceCall.valueArgumentsCount == 1) {
+            call.putValueArgument(1, sourceCall.getValueArgument(0))
+        }
 
         return call
     }
