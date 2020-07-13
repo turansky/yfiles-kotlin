46825243fa245eddcb3d96a8ca077d976f2e6e5d
Victor Turansky <victor.turansky@gmail.com>
2020-07-14 00:41:44 +0300

    Test example for 'YEnum.valueOf'

--- examples/cast/src/main/kotlin/Enum.kt
+++ examples/cast/src/main/kotlin/Enum.kt
@@ -5,7 +5,7 @@ import yfiles.layout.LayoutOrientation
 class EnumPolygon {
     fun enumTest() {
         // val values = LayoutOrientation.values()
-        // val topToBottom = LayoutOrientation.valueOf("TOP_TO_BOTTOM")
+        val topToBottom = LayoutOrientation.valueOf("TOP_TO_BOTTOM")
 
         val name = LayoutOrientation.BOTTOM_TO_TOP.name
         val ordinal = LayoutOrientation.LEFT_TO_RIGHT.ordinal
