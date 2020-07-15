a54d3ec4e605069ff755b81ef71dbc354503868d
Victor Turansky <victor.turansky@gmail.com>
2020-07-15 20:23:52 +0300

    Update enum port

--- examples/cast/src/main/kotlin/Enum.kt
+++ examples/cast/src/main/kotlin/Enum.kt
@@ -9,7 +9,13 @@ class EnumPolygon {
 
         val name = LayoutOrientation.BOTTOM_TO_TOP.name
         val ordinal = LayoutOrientation.LEFT_TO_RIGHT.ordinal
+
         val s1 = LayoutOrientation.RIGHT_TO_LEFT.toString()
-        val s2 = "${LayoutOrientation.TOP_TO_BOTTOM}"
+
+        val s2_1 = "${LayoutOrientation.TOP_TO_BOTTOM}"
+        val s2_2 = "S2_2: ${LayoutOrientation.TOP_TO_BOTTOM}"
+
+        val s3_1 = "" + LayoutOrientation.TOP_TO_BOTTOM
+        val s3_2 = "S3_2: " + LayoutOrientation.TOP_TO_BOTTOM
     }
 }
