068dc5f784353b072e96a69f052ed8e44cc22eb7
Victor Turansky <victor.turansky@gmail.com>
2020-08-03 12:25:24 +0300

    Cleanup

--- examples/cast/src/main/kotlin/FixType.kt
+++ examples/cast/src/main/kotlin/FixType.kt
@@ -1,11 +1,6 @@
 @file:Suppress("unused", "UNUSED_VARIABLE")
 
-import yfiles.geometry.Size
-import yfiles.graph.DefaultGraph
-import yfiles.graph.Tag
 import yfiles.lang.YObject
-import yfiles.styles.ShinyPlateNodeStyle
-import yfiles.styles.StringTemplatePortStyle
 
 open class BaseA : YObject {
     val x = 13
@@ -26,21 +21,21 @@ open class BaseC : YObject {
     val x = 13
     val y = 42
 
-    companion object {}
+    companion object
 }
 
 open class C : BaseC() {
     val xx = 13
     val yy = 42
 
-    companion object {}
+    companion object
 }
 
 class SuperC : C() {
     val xxx = 13
     val yyy = 42
 
-    companion object {}
+    companion object
 }
 
 @JsExport
