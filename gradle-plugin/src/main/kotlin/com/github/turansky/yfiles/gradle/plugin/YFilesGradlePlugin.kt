5f91bf347d7479ea6f67bc3c52546ad3e90f211f
Victor Turansky <victor.turansky@gmail.com>
2020-08-07 01:08:15 +0300

    Add IDEA companion configuration

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/YFilesGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/YFilesGradlePlugin.kt
@@ -5,6 +5,7 @@ import org.gradle.api.Project
 
 class YFilesGradlePlugin : Plugin<Project> {
     override fun apply(target: Project) {
+        target.configureIdeaCompanion()
         target.configureJsTransformation()
     }
 }
