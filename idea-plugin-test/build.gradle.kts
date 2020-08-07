ec29e23d1ed5a967b7bf250d7479d46d6a15df4f
Victor Turansky <victor.turansky@gmail.com>
2020-08-07 03:45:48 +0300

    yFiles 23.0.2. Update version

--- idea-plugin-test/build.gradle.kts
+++ idea-plugin-test/build.gradle.kts
@@ -17,7 +17,7 @@ kotlin {
 
 dependencies {
     implementation(kotlin("stdlib-js"))
-    implementation("com.yworks.yfiles:yfiles-kotlin:23.0.1-SNAPSHOT")
+    implementation("com.yworks.yfiles:yfiles-kotlin:23.0.2-SNAPSHOT")
 }
 
 tasks {
