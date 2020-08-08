b294367c23482232da6e827563725834b9794400
Victor Turansky <victor.turansky@gmail.com>
2020-08-08 03:29:29 +0300

    Plugin. Fix compilation

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/YFilesGradleSubplugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/YFilesGradleSubplugin.kt
@@ -2,13 +2,14 @@ package com.github.turansky.yfiles.gradle.plugin
 
 import org.gradle.api.Project
 import org.gradle.api.provider.Provider
+import org.gradle.kotlin.dsl.apply
 import org.jetbrains.kotlin.gradle.plugin.*
 
 private val YFILES_COMPILER_PLUGIN_ID = "com.github.turansky.yfiles"
 
 class YFilesGradleSubplugin : KotlinCompilerPluginSupportPlugin {
     override fun apply(target: Project) {
-        target.configureIdeaCompanion()
+        target.rootProject.plugins.apply(IdeaCompanionPlugin::class)
         target.configureJsTransformation()
     }
 
