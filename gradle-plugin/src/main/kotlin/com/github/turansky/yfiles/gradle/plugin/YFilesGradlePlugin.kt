b9114abb69e3fed706200c3a349c1b4a0bb94a3a
Victor Turansky <victor.turansky@gmail.com>
2020-08-08 03:19:58 +0300

    Plugin. Companion plugin configuration

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/YFilesGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/YFilesGradlePlugin.kt
@@ -2,10 +2,12 @@ package com.github.turansky.yfiles.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
 
 class YFilesGradlePlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        target.configureIdeaCompanion()
+        target.rootProject.plugins.apply(IdeaCompanionPlugin::class)
+
         target.configureJsTransformation()
     }
 }
