7d9579c44e9724d910f14aeadbb0ff5669c8c970
Victor Turansky <victor.turansky@gmail.com>
2020-08-14 02:29:29 +0300

    Plugin. Update IDEA companion settings

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/IdeaCompanionPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/IdeaCompanionPlugin.kt
@@ -3,12 +3,14 @@ package com.github.turansky.yfiles.gradle.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+private const val IDEA_PLUGIN_VERSION: String = "0.6.0"
+
 // language=XML
-private val EXTERNAL_DEPENDENCIES = """
+private val EXTERNAL_DEPENDENCIES: String = """
 <?xml version="1.0" encoding="UTF-8"?>
 <project version="4">
   <component name="ExternalDependencies">
-    <plugin id="com.github.turansky.yfiles" min-version="0.5.0" />
+    <plugin id="com.github.turansky.yfiles" min-version="$IDEA_PLUGIN_VERSION" />
   </component>
 </project>    
 """.trimIndent()
