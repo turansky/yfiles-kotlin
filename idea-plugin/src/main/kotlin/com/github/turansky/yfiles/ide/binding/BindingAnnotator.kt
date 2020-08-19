8a9d416a9c43806ae75fb9fdc89f9fcd2ec3f680
Victor Turansky <victor.turansky@gmail.com>
2020-08-19 11:06:04 +0300

    IDEA Plugin. Highligh only binding value

--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/binding/BindingAnnotator.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/binding/BindingAnnotator.kt
@@ -5,6 +5,7 @@ import com.github.turansky.yfiles.ide.documentation.toBinding
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.xml.XmlAttributeValue
 import org.jetbrains.kotlin.idea.highlighter.KotlinHighlightingColors
@@ -19,9 +20,12 @@ internal class BindingAnnotator : Annotator {
 
         element.value.toBinding() ?: return
 
+        val valueRange = element.textRange
+            .let { TextRange.create(it.startOffset + 1, it.endOffset - 1) }
+
         holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
             .textAttributes(KotlinHighlightingColors.SMART_CAST_VALUE)
-            .range(element)
+            .range(valueRange)
             .create()
     }
 }
