cd1b5f88c98cb126bc52179cc244e8252cbcb526
Victor Turansky <victor.turansky@gmail.com>
2020-08-20 02:01:50 +0300

    IDEA Plugin. Binding value highlight

--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/binding/BindingAnnotator.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/binding/BindingAnnotator.kt
@@ -9,6 +9,8 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.xml.XmlAttributeValue
 import org.jetbrains.kotlin.idea.highlighter.KotlinHighlightingColors
 
+private val BINDING = Regex("\\s*(Binding|TemplateBinding)\\s*(.*)\\s*")
+
 internal class BindingAnnotator : Annotator {
     override fun annotate(
         element: PsiElement,
@@ -18,25 +20,42 @@ internal class BindingAnnotator : Annotator {
         if (!element.containingFile.isSvgFile) return
 
         val value = element.value
+        val code = value.trimBraces() ?: return
         value.toBinding() ?: return
 
-        val valueRange = element.textRange
-            .let { TextRange.create(it.startOffset + 1, it.endOffset - 1) }
+        val codeStartOffset = element.textRange.startOffset + 2
 
-        val directives = BindingDirective.values()
-            .asSequence()
-            .map { it.key }
-            .filter { it in value }
-            .toList()
+        val blocks = code.split(',')
 
-        for (directive in directives) {
-            val index = value.indexOf(directive)
-            val range = TextRange.from(valueRange.startOffset + index, directive.length)
+        var localOffset = 0
+        for (block in blocks) {
+            val offset = codeStartOffset + localOffset
 
-            holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
-                .textAttributes(KotlinHighlightingColors.KEYWORD)
-                .range(range)
-                .create()
+            BINDING.find(block)?.also {
+                val keywordRange = it.groups[1]!!.range
+                holder.keyword(offset + keywordRange.first, keywordRange.count())
+
+                val dataRange = it.groups[2]!!.range
+                if (!dataRange.isEmpty()) {
+                    holder.parameter(offset + dataRange.first, dataRange.count())
+                }
+            }
+
+            localOffset += block.length + 1
         }
     }
+}
+
+private fun AnnotationHolder.keyword(offset: Int, length: Int) {
+    newSilentAnnotation(HighlightSeverity.INFORMATION)
+        .textAttributes(KotlinHighlightingColors.KEYWORD)
+        .range(TextRange.from(offset, length))
+        .create()
+}
+
+private fun AnnotationHolder.parameter(offset: Int, length: Int) {
+    newSilentAnnotation(HighlightSeverity.INFORMATION)
+        .textAttributes(KotlinHighlightingColors.PARAMETER)
+        .range(TextRange.from(offset, length))
+        .create()
 }
