41757fb14904ec4ae291bb6e80c596865153136e
Victor Turansky <victor.turansky@gmail.com>
2020-08-21 02:48:03 +0300

    IDEA Plugin. Use XML injected language highlight

--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/binding/BindingAnnotator.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/binding/BindingAnnotator.kt
@@ -4,6 +4,7 @@ import com.github.turansky.yfiles.ide.documentation.isSvgFile
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.editor.XmlHighlighterColors
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.xml.XmlAttributeValue
@@ -23,7 +24,10 @@ internal class BindingAnnotator : Annotator {
         val value = element.value
         value.toBinding() ?: return
 
-        val codeStartOffset = element.textRange.startOffset + 2
+        val valueRange = element.textRange
+        holder.languageFragment(valueRange)
+
+        val codeStartOffset = valueRange.startOffset + 2
 
         val code = value.drop(1).dropLast(1)
         val blocks = code.split(',')
@@ -66,6 +70,13 @@ internal class BindingAnnotator : Annotator {
             }
         }
     }
+}
+
+private fun AnnotationHolder.languageFragment(range: TextRange) {
+    newSilentAnnotation(HighlightSeverity.INFORMATION)
+        .textAttributes(XmlHighlighterColors.XML_INJECTED_LANGUAGE_FRAGMENT)
+        .range(range)
+        .create()
 }
 
 private fun AnnotationHolder.keyword(offset: Int, length: Int) {
