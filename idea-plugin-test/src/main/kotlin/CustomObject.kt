5ad667abd2e00367938293d8f7b56d9a3a5b45c1
Victor Turansky <victor.turansky@gmail.com>
2020-08-22 04:34:00 +0300

    IDEA Plugin. Update test example

--- idea-plugin-test/src/main/kotlin/CustomObject.kt
+++ idea-plugin-test/src/main/kotlin/CustomObject.kt
@@ -1,14 +1,20 @@
 import yfiles.lang.ClassMetadata
 import yfiles.lang.YObject
 import yfiles.styles.IArrow
-import kotlin.browser.window
+import kotlinx.browser.window
+import yfiles.lang.IClassMetadata
+import yfiles.lang.classMetadata
+
+/**
+ * [yfiles.styles.ITemplateStyleBindingContext.zoom]
+ */
 
 class CustomObject : YObject {
     fun hallo() {
         window.alert("Hallo from CustomObject!")
     }
 
-    companion object : ClassMetadata<CustomObject>
+    companion object : IClassMetadata<CustomObject> by classMetadata()
 }
 
 class OtherCustomObject : YObject {
