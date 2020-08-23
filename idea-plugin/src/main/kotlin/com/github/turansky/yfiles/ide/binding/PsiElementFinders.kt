0f563b9e32cb178dae1a0fb07ce8c125a388fec2
Victor Turansky <victor.turansky@gmail.com>
2020-08-23 23:38:46 +0300

    IDEA Plugin. Separate find class method

--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/binding/PsiElementFinders.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/binding/PsiElementFinders.kt
@@ -5,19 +5,22 @@ import com.intellij.psi.PsiElementFinder
 import org.jetbrains.kotlin.asJava.classes.KtUltraLightClass
 import org.jetbrains.kotlin.psi.psiUtil.findPropertyByName
 
+internal fun findKotlinClass(
+    context: PsiElement,
+    className: String
+): KtUltraLightClass? =
+    PsiElementFinder.EP.getExtensions(context.project)
+        .asSequence()
+        .mapNotNull { it.findClass(className, context.resolveScope) }
+        .firstOrNull() as? KtUltraLightClass
+
 internal fun findKotlinProperty(
     context: PsiElement,
     className: String,
     propertyName: String
 ): PsiElement? {
-    val psiClass = PsiElementFinder.EP.getExtensions(context.project)
-        .asSequence()
-        .mapNotNull { it.findClass(className, context.resolveScope) }
-        .firstOrNull()
+    val klass = findKotlinClass(context, className)
         ?: return null
 
-    return when (psiClass) {
-        is KtUltraLightClass -> psiClass.kotlinOrigin.findPropertyByName(propertyName)
-        else -> null
-    }
+    return klass.kotlinOrigin.findPropertyByName(propertyName)
 }
