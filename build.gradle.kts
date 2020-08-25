b78f3dd183f45621971aec5d7aa73fda1afe03e2
Victor Turansky <victor.turansky@gmail.com>
2020-08-26 00:37:35 +0300

    KFC - 0.12.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
 
 plugins {
     kotlin("js") version "1.4.0" apply false
-    id("com.github.turansky.kfc.webpack") version "0.11.0" apply false
+    id("com.github.turansky.kfc.webpack") version "0.12.1" apply false
     id("de.undercouch.download") version "4.1.1" apply false
 }
 
