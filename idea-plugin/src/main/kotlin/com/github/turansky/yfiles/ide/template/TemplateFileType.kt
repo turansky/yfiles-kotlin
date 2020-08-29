1f8b3c9d21e7149a79747dbd72a4e66beb21ae51
Victor Turansky <victor.turansky@gmail.com>
2020-08-30 02:52:50 +0300

    IDEA Plugin. Template file type - singleton

--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/template/TemplateFileType.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/template/TemplateFileType.kt
@@ -4,14 +4,9 @@ import com.intellij.ide.highlighter.XmlLikeFileType
 import com.intellij.openapi.fileTypes.UIBasedFileType
 import javax.swing.Icon
 
-class TemplateFileType private constructor() : XmlLikeFileType(TemplateLanguage), UIBasedFileType {
+object TemplateFileType : XmlLikeFileType(TemplateLanguage), UIBasedFileType {
     override fun getName(): String = TemplateLanguage.id
     override fun getDescription(): String = TemplateLanguage.id
     override fun getDefaultExtension(): String = TemplateLanguage.defaultExtension
     override fun getIcon(): Icon? = null
-
-    companion object {
-        @JvmField
-        val INSTANCE = TemplateFileType()
-    }
 }
