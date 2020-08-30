dba61b55de18c11869f9c742b03b4e6071a10d4c
Victor Turansky <victor.turansky@gmail.com>
2020-08-30 03:17:04 +0300

    IDEA Plugin. Use template language for binding checks

--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/template/TemplateFileType.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/template/TemplateFileType.kt
@@ -5,8 +5,8 @@ import com.intellij.openapi.fileTypes.UIBasedFileType
 import javax.swing.Icon
 
 object TemplateFileType : XmlLikeFileType(TemplateLanguage), UIBasedFileType {
-    override fun getName(): String = TemplateLanguage.id
-    override fun getDescription(): String = TemplateLanguage.id
+    override fun getName(): String = language.id
+    override fun getDescription(): String = language.id
     override fun getDefaultExtension(): String = TemplateLanguage.defaultExtension
     override fun getIcon(): Icon? = null
 }
