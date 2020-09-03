199346fee90c2886cfc663003f150aab41b6221f
Victor Turansky <victor.turansky@gmail.com>
2020-09-04 02:51:16 +0300

    IDEA Plugin. Add 'psiFinder' extension points

--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/psi/DefaultPsiFinder.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/psi/DefaultPsiFinder.kt
@@ -3,33 +3,27 @@ package com.github.turansky.yfiles.ide.psi
 import com.intellij.psi.PsiElement
 
 internal object DefaultPsiFinder : PsiFinder {
-    private val finders = listOf(
-        KotlinPsiFinder(),
-        JavaPsiFinder(),
-    )
+    private fun <T : Any> find(transform: (PsiFinder) -> T?): T? =
+        PsiFinder.EP_NAME.extensionList.asSequence()
+            .mapNotNull(transform)
+            .firstOrNull()
 
     override fun findClass(
         context: PsiElement,
         className: String
     ): PsiElement? =
-        finders.asSequence()
-            .mapNotNull { it.findClass(context, className) }
-            .firstOrNull()
+        find { it.findClass(context, className) }
 
     override fun findProperty(
         context: PsiElement,
         className: String,
         propertyName: String
     ): PsiElement? =
-        finders.asSequence()
-            .mapNotNull { it.findProperty(context, className, propertyName) }
-            .firstOrNull()
+        find { it.findProperty(context, className, propertyName) }
 
     override fun findPropertyVariants(
         context: PsiElement,
         classNames: List<String>
     ): Array<out Any>? =
-        finders.asSequence()
-            .mapNotNull { it.findPropertyVariants(context, classNames) }
-            .firstOrNull()
+        find { it.findPropertyVariants(context, classNames) }
 }
--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/psi/PsiElementFinders.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/psi/PsiElementFinders.kt
@@ -1,6 +1,8 @@
 package com.github.turansky.yfiles.ide.psi
 
+import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.psi.PsiElement
+
 
 interface PsiFinder {
     fun findClass(
@@ -18,5 +20,9 @@ interface PsiFinder {
         context: PsiElement,
         classNames: List<String>
     ): Array<out Any>? = null
+
+    companion object {
+        val EP_NAME: ExtensionPointName<PsiFinder> = ExtensionPointName.create("com.github.turansky.yfiles.psiFinder")
+    }
 }
 
