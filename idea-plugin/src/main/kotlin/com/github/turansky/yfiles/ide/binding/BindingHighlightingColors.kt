48479fedea1bd25e11dcf7948c2acd8f593d9f4e
Victor Turansky <victor.turansky@gmail.com>
2020-09-03 11:08:28 +0300

    IDEA Plugin. Update binding color scheme

--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/binding/BindingHighlightingColors.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/binding/BindingHighlightingColors.kt
@@ -2,17 +2,16 @@ package com.github.turansky.yfiles.ide.binding
 
 import com.intellij.openapi.editor.XmlHighlighterColors
 import com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey
-import org.jetbrains.kotlin.idea.highlighter.KotlinHighlightingColors
 
 object BindingHighlightingColors {
     val LANGUAGE_INJECTION = createTextAttributesKey("BINDING_LANGUAGE_INJECTION", XmlHighlighterColors.XML_INJECTED_LANGUAGE_FRAGMENT)
     val BRACE = createTextAttributesKey("BINDING_BRACE", XmlHighlighterColors.XML_ATTRIBUTE_NAME)
     val KEYWORD = createTextAttributesKey("BINDING_KEYWORD", XmlHighlighterColors.XML_NS_PREFIX)
 
-    val NAMED_ARGUMENT = createTextAttributesKey("BINDING_NAMED_ARGUMENT", KotlinHighlightingColors.NAMED_ARGUMENT)
+    val NAMED_ARGUMENT = createTextAttributesKey("BINDING_NAMED_ARGUMENT", KEYWORD)
     val ASSIGN = createTextAttributesKey("BINDING_ASSIGN", NAMED_ARGUMENT)
 
-    val ARGUMENT = createTextAttributesKey("BINDING_ARGUMENT", KotlinHighlightingColors.STRING_ESCAPE)
+    val ARGUMENT = createTextAttributesKey("BINDING_ARGUMENT", XmlHighlighterColors.XML_ATTRIBUTE_NAME)
     val VALUE = createTextAttributesKey("BINDING_VALUE", XmlHighlighterColors.XML_ATTRIBUTE_VALUE)
     val COMMA = createTextAttributesKey("BINDING_COMMA", ARGUMENT)
 }
