79e72931a7f430d202a2bd659977b3a2752d17c0
Victor Turansky <victor.turansky@gmail.com>
2020-09-12 05:18:56 +0300

    IDEA Plugin. Add binding parse method

--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/binding/BindingParser.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/binding/BindingParser.kt
@@ -3,8 +3,8 @@ package com.github.turansky.yfiles.ide.binding
 import com.github.turansky.yfiles.ide.binding.BindingDirective.*
 
 internal object BindingParser {
-    val KEYWORD = Regex("\\s*($BINDING|$TEMPLATE_BINDING)\\s*(\\S*)\\s*")
-    val ARGUMENT = Regex("\\s*($CONVERTER|$PARAMETER)\\s*(=)\\s*(\\S*)\\s*")
+    private val KEYWORD = Regex("\\s*($BINDING|$TEMPLATE_BINDING)\\s*(\\S*)\\s*")
+    private val ARGUMENT = Regex("\\s*($CONVERTER|$PARAMETER)\\s*(=)\\s*(\\S*)\\s*")
 
     fun find(source: String): Pair<BindingDirective, String?>? =
         when {
@@ -20,4 +20,41 @@ internal object BindingParser {
 
             else -> null
         }
+
+    fun parse(source: String): List<BindingParseResult> {
+        KEYWORD.find(source)?.also { result ->
+            val keywordRange = result.r(1)
+            val argumentRange = result.r(2)
+
+            return if (argumentRange.isEmpty()) {
+                listOf(
+                    BindingParseResult(BindingToken.KEYWORD, keywordRange)
+                )
+            } else {
+                listOf(
+                    BindingParseResult(BindingToken.KEYWORD, keywordRange),
+                    BindingParseResult(BindingToken.ARGUMENT, argumentRange)
+                )
+            }
+        }
+
+        ARGUMENT.find(source)?.also { result ->
+            val argumentMode = result.d(1) == CONVERTER
+
+            return listOf(
+                BindingParseResult(BindingToken.KEYWORD, result.r(1)),
+                BindingParseResult(BindingToken.ASSIGN, result.r(2)),
+                BindingParseResult(if (argumentMode) BindingToken.ARGUMENT else BindingToken.VALUE, result.r(3))
+            )
+        }
+
+        return listOf(
+            BindingParseResult(BindingToken.ERROR, source.indices)
+        )
+    }
 }
+
+internal data class BindingParseResult(
+    val token: BindingToken,
+    val range: IntRange
+)
---
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/binding/BindingToken.kt
@@ -0,0 +1,10 @@
+package com.github.turansky.yfiles.ide.binding
+
+internal enum class BindingToken {
+    KEYWORD,
+    ASSIGN,
+    ARGUMENT,
+    VALUE,
+
+    ERROR
+}
