e0b0e4a0526583970c803bb2cac0fe042aaf87a7
Victor Turansky <victor.turansky@gmail.com>
2020-09-26 02:36:44 +0300

    Add delegate examples

--- examples/import-optimizer-application/src/main/kotlin/Components.kt
+++ examples/import-optimizer-application/src/main/kotlin/Components.kt
@@ -1,3 +1,4 @@
+import yfiles.algorithms.nodeDpKey
 import yfiles.view.GraphComponent
 
 @JsExport
@@ -6,3 +7,11 @@ fun createComponent(): GraphComponent =
     GraphComponent {
         graph = createGraph()
     }
+
+@JsExport
+@ExperimentalJsExport
+val STRING_DATA_KEY by nodeDpKey<String>()
+
+@JsExport
+@ExperimentalJsExport
+val INT_DATA_KEY by nodeDpKey<Int>()
