247bfbede373806dd2e939559fccc01ebb445c50
Victor Turansky <victor.turansky@gmail.com>
2020-10-01 22:43:51 +0300

    [GP] Update ConfigurablePropertyPlugin

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/ConfigurablePropertyPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/ConfigurablePropertyPlugin.kt
@@ -1,85 +1,45 @@
+// TODO: Remove after fix https://youtrack.jetbrains.com/issue/KT-42364
+
 package com.github.turansky.yfiles.gradle.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.Copy
-import org.gradle.api.tasks.TaskContainer
-import org.gradle.api.tasks.TaskProvider
-import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
-import org.jetbrains.kotlin.gradle.plugin.KotlinJsPluginWrapper
-import org.jetbrains.kotlin.gradle.tasks.KotlinJsDce
-import java.io.File
 
 private object KotlinJs {
+    val PLUGIN_ID = "org.jetbrains.kotlin.js"
+
     val COMPILE_TASK_NAMES = setOf(
         "compileKotlinJs",
         "compileTestKotlinJs"
     )
 
     val TARGET_V5 = "v5"
-
-    val OUTPUT_FILE = "outputFile"
 }
+
+private val DESCRIPTOR_REGEX = Regex("(Object\\.defineProperty\\(.+\\.prototype, '[a-zA-Z]+', \\{)(\\n\\s+get:)")
 
 internal class ConfigurablePropertyPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit = with(target) {
-        plugins.withType<KotlinJsPluginWrapper> {
-            // wait for Kotlin target configuration
-            afterEvaluate {
-                // "freeze" DCE source file list
-                tasks.withType<KotlinJsDce>()
-                    .forEach { it.source.files }
-
-                val compileTasks = tasks.withType<KotlinJsCompile>()
-                    .filter { it.name in KotlinJs.COMPILE_TASK_NAMES }
-
-                for (compileTask in compileTasks) {
-                    val jsTarget = compileTask.kotlinOptions.target
+        plugins.withId(KotlinJs.PLUGIN_ID) {
+            tasks.withType<KotlinJsCompile>().configureEach {
+                doLast {
+                    val jsTarget = kotlinOptions.target
                     if (jsTarget != KotlinJs.TARGET_V5) {
-                        logger.warn("Unsupported JS target '$jsTarget'. Fix for KT-34770 won't be applied!")
-                        continue
-                    }
+                        logger.warn("Unsupported JS target '$jsTarget'. Fix for KT-42364 won't be applied!")
+                    } else if (name in KotlinJs.COMPILE_TASK_NAMES) {
+                        val outputFile = file(kotlinOptions.outputFile!!)
+                        val content = outputFile.readText()
+                        val newContent = content
+                            .replace(DESCRIPTOR_REGEX, "$1\n    configurable: true,$2")
 
-                    val config = compileTask.addJsTransformation()
-                    val copyTask = tasks.copyTransformedJs(compileTask.name, config)
-                    compileTask.finalizedBy(copyTask)
+                        if (newContent != content) {
+                            outputFile.writeText(newContent)
+                        }
+                    }
                 }
             }
         }
     }
-}
-
-private fun KotlinJsCompile.addJsTransformation(): TransformationConfig =
-    TransformationConfig(property(KotlinJs.OUTPUT_FILE) as File).apply {
-        kotlinOptions.outputFile = tempOutputFile.absolutePath
-    }
-
-private fun TaskContainer.copyTransformedJs(
-    compileTaskName: String,
-    config: TransformationConfig
-): TaskProvider<*> =
-    register("copyTransformedJs_$compileTaskName", Copy::class) {
-        val outputFileName = config.originalOutputFile.name
-
-        from(config.tempOutputDir) {
-            include(outputFileName)
-            filter { line -> line.fixPropertyDeclaration() }
-        }
-
-        from(config.tempOutputDir) {
-            exclude(outputFileName)
-        }
-
-        into(config.originalOutputDir)
-    }
-
-private class TransformationConfig(val originalOutputFile: File) {
-    val originalOutputDir: File = originalOutputFile.parentFile
-
-    val tempOutputDir: File = originalOutputDir.parentFile
-        .resolve("${originalOutputDir.name}-temp")
-
-    val tempOutputFile: File = tempOutputDir.resolve(originalOutputFile.name)
 }
