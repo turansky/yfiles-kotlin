1c0ab0731b3a989c96f1400d2c5f4df1ff00620c
Victor Turansky <victor.turansky@gmail.com>
2020-10-04 15:17:31 +0300

    [GP] Version - 4.10.0

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/IdeaCompanionPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/IdeaCompanionPlugin.kt
@@ -3,7 +3,7 @@ package com.github.turansky.yfiles.gradle.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-private const val IDEA_PLUGIN_VERSION: String = "0.24.0"
+private const val IDEA_PLUGIN_VERSION: String = "0.25.0"
 
 // language=XML
 private val EXTERNAL_DEPENDENCIES: String = """
