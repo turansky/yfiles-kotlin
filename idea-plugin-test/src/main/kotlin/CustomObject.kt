3d2b37d603114561fe3915be8569e65c728da0a5
Victor Turansky <victor.turansky@gmail.com>
2020-10-07 13:08:06 +0300

    [IP] Add color test

--- idea-plugin-test/src/main/kotlin/CustomObject.kt
+++ idea-plugin-test/src/main/kotlin/CustomObject.kt
@@ -4,6 +4,7 @@ import yfiles.styles.IArrow
 import kotlinx.browser.window
 import yfiles.lang.IClassMetadata
 import yfiles.lang.classMetadata
+import yfiles.view.Color
 
 /**
  * [yfiles.styles.ITemplateStyleBindingContext.zoom]
@@ -24,3 +25,9 @@ class OtherCustomObject : YObject {
 }
 
 abstract class AbstractArrow : IArrow {}
+
+fun colorTest() {
+    val color1 = Color("#FF0000")
+    val color2 = Color("#00FF00")
+    val color3 = Color("#0000FF")
+}
