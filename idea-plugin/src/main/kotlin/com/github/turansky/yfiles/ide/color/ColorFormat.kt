8d3715578885f213ac9435b845eaa9ed45442ab7
Victor Turansky <victor.turansky@gmail.com>
2020-10-08 03:35:06 +0300

    [IP] Support 'Short HEX' color format

--- idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/color/ColorFormat.kt
+++ idea-plugin/src/main/kotlin/com/github/turansky/yfiles/ide/color/ColorFormat.kt
@@ -4,7 +4,24 @@ import com.intellij.xml.util.ColorMap
 import java.awt.Color
 
 enum class ColorFormat {
-    RGB {
+    SHORT_HEX {
+        private val PATTERN = Regex("#[A-Fa-f0-9]{3}")
+        private val LENGTH = 4
+
+        override fun matches(color: String): Boolean =
+            color.length == LENGTH && PATTERN.matches(color)
+
+        override fun parse(color: String): Color {
+            val code = color.substring(1).toInt(16)
+            return Color(
+                (code shr 8 and 15) * 17,
+                (code shr 4 and 15) * 17,
+                (code and 15) * 17
+            )
+        }
+    },
+
+    HEX {
         private val PATTERN = Regex("#[A-Fa-f0-9]{6}")
         private val LENGTH = 7
 
