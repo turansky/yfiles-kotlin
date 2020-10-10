fdfb678bf9ba38bcc5aa801a960a54a20ca5af4b
Victor Turansky <victor.turansky@gmail.com>
2020-10-10 20:18:35 +0300

    [IP] Add palette example

---
+++ idea-plugin-test/src/main/kotlin/MaterialPalette.kt
@@ -0,0 +1,32 @@
+@file:Suppress("unused")
+
+import yfiles.view.Color
+
+object PrimaryColor {
+    val RED: Color = Color("#F44336")
+    val PINK: Color = Color("#E91E63")
+    val PURPLE: Color = Color("#9C27B0")
+    val DEEP_PURPLE: Color = Color("#673AB7")
+    val INDIGO: Color = Color("#3F51B5")
+    val BLUE: Color = Color("#2196F3")
+    val LIGHT_BLUE: Color = Color("#03A9F4")
+    val CYAN: Color = Color("#00BCD4")
+    val TEAL: Color = Color("#009688")
+    val GREEN: Color = Color("#4CAF50")
+    val LIGHT_GREEN: Color = Color("#8BC34A")
+    val LIME: Color = Color("#CDDC39")
+    val YELLOW: Color = Color("#FFEB3B")
+    val AMBER: Color = Color("#FFC107")
+    val ORANGE: Color = Color("#FF9800")
+    val DEEP_ORANGE: Color = Color("#FF5722")
+    val BROWN: Color = Color("#795548")
+    val GREY: Color = Color("#9E9E9E")
+    val BLUE_GREY: Color = Color("#607D8B")
+}
+
+
+
+
+
+
+
