f2da33cedbaf55c020d915e73a90814c0a614f58
Victor Turansky <victor.turansky@gmail.com>
2020-10-10 12:08:10 +0300

    [IP] Add fill constants to example

--- idea-plugin-test/src/main/kotlin/CustomObject.kt
+++ idea-plugin-test/src/main/kotlin/CustomObject.kt
@@ -7,6 +7,7 @@ import yfiles.view.Color
 import yfiles.view.Color.Companion.BLUE
 import yfiles.view.Color.Companion.LIME
 import yfiles.view.Color.Companion.RED
+import yfiles.view.Fill
 
 /**
  * [yfiles.styles.ITemplateStyleBindingContext.zoom]
@@ -27,6 +28,9 @@ class OtherCustomObject : YObject {
 }
 
 abstract class AbstractArrow : IArrow {}
+
+class Color2(val source: String) {}
+class Fill2(val source: String) {}
 
 fun colorTest() {
     val color_01_1 = Color("red")
@@ -56,6 +60,36 @@ fun colorTest() {
     val color_72_1 = RED
     val color_72_2 = LIME
     val color_72_3 = BLUE
+}
+
+fun fillTest() {
+    val fill_01_1 = Fill("red")
+    val fill_01_2 = Fill("lime")
+    val fill_01_3 = Fill("blue")
+
+    val fill_11_1 = Fill("#F00")
+    val fill_11_2 = Fill("#0F0")
+    val fill_11_3 = Fill("#00F")
+
+    val fill_12_1 = Fill("#FF0000")
+    val fill_12_2 = Fill("#00FF00")
+    val fill_12_3 = Fill("#0000FF")
+
+    val fill_21_1 = Fill("rgb(255, 0, 0)")
+    val fill_21_2 = Fill("rgb(0, 255, 0)")
+    val fill_21_3 = Fill("rgb(0, 0, 255)")
+
+    val fill_31_1 = Fill("hsl(0, 100%, 50%)")
+    val fill_31_2 = Fill("hsl(120, 100%, 50%)")
+    val fill_31_3 = Fill("hsl(240, 100%, 50%)")
+
+    val fill_71_1 = Fill.RED
+    val fill_71_2 = Fill.LIME
+    val fill_71_3 = Fill.BLUE
+
+    val fill_72_1 = RED
+    val fill_72_2 = LIME
+    val fill_72_3 = BLUE
 }
 
 // language=CSS
