4d5302d6ad3f9679a50dd9a0964d8381b18623e1
Victor Turansky <victor.turansky@gmail.com>
2020-10-10 13:05:14 +0300

    [IP] Update test build script

--- idea-plugin-test/build.gradle.kts
+++ idea-plugin-test/build.gradle.kts
@@ -2,7 +2,8 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
 
 plugins {
     kotlin("js") version "1.4.10"
-    id("com.github.turansky.yfiles") version "3.2.0"
+    id("com.github.turansky.kfc.library") version "0.16.0"
+    id("com.github.turansky.yfiles") version "4.12.0"
 }
 
 repositories {
@@ -11,24 +12,11 @@ repositories {
     mavenLocal()
 }
 
-kotlin.js {
-    browser()
-}
-
 dependencies {
     implementation("com.yworks.yfiles:yfiles-kotlin:23.0.2-SNAPSHOT")
 }
 
-tasks {
-    withType<KotlinJsCompile>().configureEach {
-        kotlinOptions {
-            moduleKind = "commonjs"
-            allWarningsAsErrors = true
-        }
-    }
-
-    wrapper {
-        gradleVersion = "6.6.1"
-        distributionType = Wrapper.DistributionType.ALL
-    }
+tasks.wrapper {
+    gradleVersion = "6.6.1"
+    distributionType = Wrapper.DistributionType.ALL
 }
