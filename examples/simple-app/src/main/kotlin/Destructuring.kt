8007273f90d8711fd1c0abe7759637b31641de32
Victor Turansky <victor.turansky@gmail.com>
2020-10-23 21:24:43 +0300

    Update 'destructuring' example

--- examples/simple-app/src/main/kotlin/Destructuring.kt
+++ examples/simple-app/src/main/kotlin/Destructuring.kt
@@ -2,6 +2,7 @@
 
 import yfiles.algorithms.YPoint
 import yfiles.algorithms.YPoint.Companion.plus
+import yfiles.collections.*
 
 fun destructuring() {
     val p1 = YPoint(4.0, 8.0)
@@ -9,4 +10,10 @@ fun destructuring() {
     val p3 = YPoint(23.0, 42.0)
 
     val (x, y) = p1 + p2 + p3
+
+    val l: IList<String> = List()
+    val (l1, l2, l3) = l
+
+    val e: IEnumerable<String> = l
+    val (e1, e2, e3) = e
 }
