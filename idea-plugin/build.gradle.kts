f32310083630cbc9cf69dc7da468b14a3bdfcd81
Victor Turansky <victor.turansky@gmail.com>
2020-11-17 10:14:18 +0300

    [IP] org.jetbrains.intellij - 0.6.3

--- idea-plugin/build.gradle.kts
+++ idea-plugin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.4.10"
-    id("org.jetbrains.intellij") version "0.6.1"
+    id("org.jetbrains.intellij") version "0.6.3"
     id("com.github.turansky.kfc.version") version "1.0.0"
 }
 
