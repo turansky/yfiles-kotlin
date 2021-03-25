875f4a870260a60ebaf29277f9f2beecde13a8ad
Victor Turansky <victor.turansky@gmail.com>
2021-03-25 15:58:31 +0300

    [GP] Version - 6.3.0

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/KotlinPluginArtifact.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/KotlinPluginArtifact.kt
@@ -6,5 +6,5 @@ internal val KOTLIN_PLUGIN_ARTIFACT: SubpluginArtifact
     get() = SubpluginArtifact(
         groupId = "gradle.plugin.com.github.turansky.yfiles",
         artifactId = "gradle-plugin",
-        version = "6.2.1-SNAPSHOT"
+        version = "6.3.1-SNAPSHOT"
     )
