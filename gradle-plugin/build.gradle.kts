489654d4fc18a5528fa4ab01d9bd43616948cba7
Victor Turansky <victor.turansky@gmail.com>
2021-03-25 21:23:04 +0300

    Fix KFC plugin version

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     `kotlin-dsl`
 
     id("com.gradle.plugin-publish") version "0.13.0"
-    id("com.github.turansky.kfc.plugin-publish") version "2.0.0"
+    id("com.github.turansky.kfc.plugin-publish") version "3.0.0"
 
     kotlin("jvm") version "1.4.32"
 }
