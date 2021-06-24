72c782277a1059abe7008c82f2dfa0b4eb46aa5d
Victor Turansky <victor.turansky@gmail.com>
2021-06-25 02:17:45 +0300

    [GP] Version - `6.7.0`

--- idea-plugin-test/build.gradle.kts
+++ idea-plugin-test/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("js") version "1.5.20"
     id("com.github.turansky.kfc.library") version "4.12.0"
-    id("com.github.turansky.yfiles") version "6.6.0"
+    id("com.github.turansky.yfiles") version "6.7.0"
 }
 
 repositories {
