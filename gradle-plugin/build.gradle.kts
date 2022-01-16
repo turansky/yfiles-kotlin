82774b4e87f348cdda69ed2d3af023cd99b4c59f
Victor Turansky <victor.turansky@gmail.com>
2022-01-17 01:20:18 +0300

    KFC `4.80.0`

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     `kotlin-dsl`
 
     id("com.gradle.plugin-publish") version "0.19.0"
-    id("com.github.turansky.kfc.plugin-publish") version "4.70.0"
+    id("com.github.turansky.kfc.plugin-publish") version "4.80.0"
 
     kotlin("jvm") version "1.6.10"
 }
