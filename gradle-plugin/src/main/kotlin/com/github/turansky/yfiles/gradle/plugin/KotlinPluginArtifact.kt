ca75d8a46ea229161633358d393f038cf045002a
Victor Turansky <victor.turansky@gmail.com>
2022-07-17 15:03:44 +0300

    Remove gradle prefix logic

--- gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/KotlinPluginArtifact.kt
+++ gradle-plugin/src/main/kotlin/com/github/turansky/yfiles/gradle/plugin/KotlinPluginArtifact.kt
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.plugin.SubpluginArtifact
 
 internal val KOTLIN_PLUGIN_ARTIFACT: SubpluginArtifact
     get() = SubpluginArtifact(
-        groupId = "gradle.plugin.com.github.turansky.yfiles",
+        groupId = "com.github.turansky.yfiles",
         artifactId = "gradle-plugin",
         version = "6.11.1-SNAPSHOT"
     )
