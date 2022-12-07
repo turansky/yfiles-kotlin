25e7474f0fb888a94a8921675c1934fd44cf49ae
Victor Turansky <victor.turansky@gmail.com>
2022-12-07 03:37:34 +0200

    [IP] IDEA `2022.3`

--- idea-plugin/build.gradle.kts
+++ idea-plugin/build.gradle.kts
@@ -12,7 +12,7 @@ intellij {
     pluginName.set("yfiles")
 
     type.set("IU")
-    version.set("2022.2.3")
+    version.set("2022.3")
 
     plugins.set(
         listOf(
@@ -37,7 +37,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("222.3345")
-        untilBuild.set("222.*")
+        untilBuild.set("224.*")
     }
 
     publishPlugin {
